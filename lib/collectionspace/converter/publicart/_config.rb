86667cffdf7f9afce031c6a4873abf5c0a94f760
Mark Cooper <markchristophercooper@gmail.com>
2019-10-23 15:19:20 -0700

    Update publicart config

--- lib/collectionspace/converter/publicart/_config.rb
+++ lib/collectionspace/converter/publicart/_config.rb
@@ -1,191 +1,25 @@
 module CollectionSpace
   module Converter
     module PublicArt
+      def self.config
+        @config ||= YAML.safe_load(
+          File.open(
+            File.expand_path('config.yml', __dir__)
+          )
+        )
+      end
+
+      def self.registered_authorities
+        config['registered_authorities']
+      end
 
       def self.registered_procedures
-        [
-          "CollectionObject",
-          "Exhibition",
-          "LoanIn",
-          "LoanOut",
-          "Media",
-          "Intake",
-          "Acquisition",
-          "Group",
-          "ValuationControl",
-          "Movement",
-          "ConditionCheck",
-          "ObjectExit",
-        ]
+        config['registered_procedures']
       end
 
       def self.registered_profiles
-        {
-          "cataloging" => {
-            "Procedures" => {
-              "CollectionObject" => {
-                "identifier_field" => "objectNumber",
-                "identifier" => "objectnumber",
-                "title" => "title",
-              },
-            },
-            "Authorities" => {
-              "Person" => ["content_person", "inscriber", "objectproductionperson", "owners_person"],
-              "Organization" => ["production_org", "owners_org"],
-              "Concept" => [["material", "material_ca"]],
-            },
-            "Relationships" => [
-              {
-              },
-            ],
-          },
-          "conditioncheck" => {
-            "Procedures" => {
-              "ConditionCheck" => {
-                "identifier_field" => "conditionCheckRefNumber",
-                "identifier" => "condition_check_reference_number",
-                "title" => "condition_check_reference_number",
-               },
-             },
-             "Authorities" => {
-               "Person" => ["condition_checker"],
-             },
-             "Relationships" => [],
-           },
-          "exhibition" => {
-            "Procedures" => {
-              "Exhibition" => {
-                "identifier_field" => "exhibitionNumber",
-                "identifier" => "exhibition_number",
-                "title" => "exhibition_number",
-              },
-            },
-            "Authorities" => {
-              "Organization" => ["organizer"],
-            },
-            "Relationships" => [],
-          },
-          "group" => {
-            "Procedures" => {
-              "Group" => {
-                "identifier_field" => "title",
-                "identifier" => "title",
-                "title" => "title",
-              },
-            },
-            "Authorities" => {
-              "Person" => ["owner"],
-            },
-            "Relationships" => [],
-          },
-          "loanin" => {
-            "Procedures" => {
-              "LoanIn" => {
-                "identifier_field" => "loanInNumber",
-                "identifier" => "loan_in_number",
-                "title" => "loan_in_number",
-               },
-             },
-             "Authorities" => {
-               "Person" => ["lender's_authorizer", "borrower's_authorizer"],
-               "Organization" => ["lender"],
-             },
-             "Relationships" => [],
-           },
-           "loanout" => {
-             "Procedures" => {
-               "LoanOut" => {
-                 "identifier_field" => "loanOutNumber",
-                 "identifier" => "loan_out_number",
-                 "title" => "loan_out_number",
-               },
-             },
-             "Authorities" => {
-               "Person" => ["lender's_authorizer", "borrower's_authorizer"],
-               "Organization" => ["borrower"],
-             },
-             "Relationships" => [],
-           },
-           "media" => {
-             "Procedures" => {
-               "Media" => {
-                 "identifier_field" => "identificationNumber",
-                 "identifier" => "identificationnumber",
-                 "title" => "identificationnumber",
-               },
-             },
-             "Authorities" => {
-             },
-             "Relationships" => [],
-           },
-           "movement" => {
-             "Procedures" => {
-               "Movement" => {
-                 "identifier_field" => "movementReferenceNumber",
-                 "identifier" => "movementreferencenumber",
-                 "title" => "movementreferencenumber",
-               },
-             },
-             "Authorities" => {
-               "Person" => ["movement_contact"],
-             },
-             "Relationships" => [],
-           },
-           "intake" => {
-             "Procedures" => {
-               "Intake" => {
-                 "identifier_field" => "entryNumber",
-                 "identifier" => "intake_entry_number",
-                 "title" => "intake_entry_number",
-               },
-             },
-             "Authorities" => {
-              "Person" => ["current_owner"],
-             },
-             "Relationships" => [],
-           },
-           "objectexit" => {
-             "Procedures" => {
-               "ObjectExit" => {
-                 "identifier_field" => "exitNumber",
-                 "identifier" => "exit_number",
-                 "title" => "exit_number",
-               },
-             },
-             "Authorities" => {
-              "Organization" => ["current_owner"],
-             },
-             "Relationships" => [],
-           },
-           "acquisition" => {
-             "Procedures" => {
-               "Acquisition" => {
-                 "identifier_field" => "acquisitionReferenceNumber",
-                 "identifier" => "acquisition_reference_number",
-                 "title" => "acquisition_reference_number",
-               },
-             },
-             "Authorities" => {
-              "Person" => ["acquisition_authorizer", "owner"],
-            },
-            "Relationships" => [],
-          },
-          "valuationcontrol" => {
-             "Procedures" => {
-               "ValuationControl" => {
-                 "identifier_field" => "valuationcontrolRefNumber",
-                 "identifier" => "valuation_control_reference_number",
-                 "title" => "valuation_control_reference_number",
-               },
-             },
-             "Authorities" => {
-              "Person" => ["source"],
-            },
-            "Relationships" => [],
-          },
-        }
+        config['registered_profiles']
       end
-
     end
   end
 end
