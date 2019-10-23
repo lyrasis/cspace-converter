3f8b6608e288e3b8e8097e337e87eff0ae4dc146
Mark Cooper <markchristophercooper@gmail.com>
2019-10-23 14:23:37 -0700

    Remove unused auth check

--- lib/collectionspace/converter/default/default.rb
+++ lib/collectionspace/converter/default/default.rb
@@ -21,16 +21,9 @@ module CollectionSpace
         config['registered_profiles']
       end
 
-      def self.validate_authority!(authority)
-        unless [ "Concept", "Location", "Material", "Materials", "Person", "Place", "Organization", "Taxon", "Work" ].include? authority
-          raise "Invalid authority #{authority}"
-        end
-      end
-
-      # set which procedures can be created from model
       def self.validate_procedure!(procedure, converter)
         valid_procedures = converter.registered_procedures
-        unless valid_procedures.include?("all") or valid_procedures.include?(procedure)
+        unless valid_procedures.include?(procedure)
           raise "Invalid procedure #{procedure}, not permitted by configuration."
         end
       end
