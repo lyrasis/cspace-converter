14641c27a82564d334a9aad183f9dd8ae467ac7c
Paul Mulonzia <paulmwendwa43@gmail.com>
2019-10-29 14:29:43 +0000

    media profile updates

--- lib/collectionspace/converter/core/media.rb
+++ lib/collectionspace/converter/core/media.rb
@@ -12,9 +12,13 @@ module CollectionSpace
         def self.map(xml, attributes)
           CSXML.add xml, 'identificationNumber', attributes["identificationnumber"]
           CSXML.add xml, 'title', attributes["title"]
-          CSXML::Helpers.add_person xml, 'contributor', attributes["contributor"]
+          CSXML::Helpers.add_person xml, 'contributor', attributes["contributor"] if attributes["contributortype"] == "person"
+          CSXML::Helpers.add_organization xml, 'contributor', attributes["contributor"] if attributes["contributortype"] == "organization"
           CSXML.add xml, 'copyrightStatement', attributes["copyrightstatement"]
           CSXML.add xml, 'coverage', attributes["coverage"]
+          CSXML::Helpers.add_person xml, 'creator', attributes["creator"] if attributes["creatortype"] == "person"
+          CSXML::Helpers.add_organization xml, 'creator', attributes["creator"] if attributes["creatortype"] == "organization"
+          CSXML.add xml, 'description', scrub_fields([attributes["description"]])
           # measuredPartGroupList
           overall_data = {
             "measuredPart" => attributes["measuredpart"],
@@ -32,9 +36,17 @@ module CollectionSpace
           dims.each_with_index do |dim, index|
             dimensions << { "dimension" => dim, "value" => values[index], "measurementUnit" => unit, "measuredBy" => by, "measurementMethod" => method, "valueDate" => date, "valueQualifier" => qualifier, "dimensionNote" => note}
           end
-          CSXML.add_group_list xml, 'measuredPart', [overall_data], 'dimension', dimensions
-          CSXML.add xml, 'description', scrub_fields([attributes["description"]])
-          CSXML.add xml, 'externalUrl', attributes["externalurl"]
+          CSXML.add_group_list xml, 'measuredPart', [overall_data], 'dimension', dimensions 
+          CSXML.add_list xml, 'language', [{'language' => attributes['language']}]
+          CSXML::Helpers.add_person xml, 'publisher', attributes["publisher"] if attributes["publishertype"] == "person"
+          CSXML::Helpers.add_organization xml, 'publisher', attributes["publisher"] if attributes["publishertype"] == "organization"
+          CSXML.add_list xml, 'relation', [{'relation' => attributes['relation']}] 
+          CSXML::Helpers.add_person xml, 'rightsHolder', attributes["rightsholder"] if attributes["rightsholdertype"] == "person"
+          CSXML::Helpers.add_organization xml, 'rightsHolder', attributes["rightsholder"] if attributes["rightsholdertype"] == "organization"
+          CSXML.add xml, 'source', attributes["source"]
+          CSXML.add xml, 'externalUrl', attributes["sourceurl"]
+          CSXML.add_list xml, 'subject', [{'subject' => attributes['subject']}]
+          CSXML.add_list xml, 'type', [{'type' => attributes['type']}]   
         end
       end
     end
