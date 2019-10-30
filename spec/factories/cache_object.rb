2ee917c0a427c72520460fb38132044d08a9f1de
Mark Cooper <markchristophercooper@gmail.com>
2019-10-30 10:11:21 -0700

    Bundle update 103019

--- spec/factories/cache_object.rb
+++ spec/factories/cache_object.rb
@@ -1,12 +1,12 @@
-FactoryGirl.define do
+FactoryBot.define do
 
   factory :cache_object do
-    refname "urn:cspace:core.collectionspace.org:orgauthorities:name(organization):item:name(BarnesFoundation1542642516661)'Barnes Foundation'"
-    name 'Barnes Foundation'
-    identifier 'BarnesFoundation1542642516661'
-    rev 0
-    parent_refname "urn:cspace:core.collectionspace.org:orgauthorities:name(organization)'Local Organizations'"
-    parent_rev 0
+    refname { "urn:cspace:core.collectionspace.org:orgauthorities:name(organization):item:name(BarnesFoundation1542642516661)'Barnes Foundation'" }
+    name { 'Barnes Foundation' }
+    identifier { 'BarnesFoundation1542642516661' }
+    rev { 0 }
+    parent_refname { "urn:cspace:core.collectionspace.org:orgauthorities:name(organization)'Local Organizations'" }
+    parent_rev { 0 }
   end
 
 end
--- spec/factories/data_object.rb
+++ spec/factories/data_object.rb
@@ -1,10 +1,10 @@
-FactoryGirl.define do
+FactoryBot.define do
 
   factory :data_object do
-    converter_profile 'cataloging'
-    import_batch 'cataloging1'
-    import_category 'Procedures'
-    import_file 'cataloging1.csv'
+    converter_profile { 'cataloging' }
+    import_batch { 'cataloging1' }
+    import_category { 'Procedures' }
+    import_file { 'cataloging1.csv' }
     object_data { { id: '1' } }
   end
 
