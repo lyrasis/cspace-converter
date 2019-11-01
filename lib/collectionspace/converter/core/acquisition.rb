9929842f93ea779751a586a94027e3896cca5c39
Paul Mulonzia <paulmwendwa43@gmail.com>
2019-11-01 15:48:12 +0000

    acquisition converter profile updates

--- lib/collectionspace/converter/core/acquisition.rb
+++ lib/collectionspace/converter/core/acquisition.rb
@@ -11,35 +11,73 @@ module CollectionSpace
 
         def self.map(xml, attributes)
           #acquisitionReferenceNumber
-          CSXML.add xml, 'acquisitionReferenceNumber', attributes["acquisition_reference_number"]
-
+          CSXML.add xml, 'acquisitionReferenceNumber', attributes["acquisitionreferencenumber"]
+=begin
           #accessionDateGroup
-          CSXML.add_group xml, 'accessionDate', { "dateDisplayDate" => attributes['accession_display_date'],
-            'dateEarliestSingleYear' => attributes['accession_earliest_/_single_date_year'],
-            'dateEarliestSingleMonth' => attributes['accession_earliest_/_single_date_month'],
-            'dateEarliestSingleDay' => attributes['accession_earliest_/_single_date_day'],
-            'dateLatestYear' => attributes['accession_latest_date_year'],
-            'dateLatestMonth' => attributes['accession_latest_date_month'],
-            'dateLatestDay' => attributes['accession_latest_date_day'],
-          }
-
+          CSXML.add_group_list xml, 'accessionDate', [{
+           "dateDisplayDate" => attributes["accessiondategroup"],
+           "dateEarliestScalarValue" => CSDTP.parse(attributes["accessiondategroup"]).earliest_scalar,
+          }]
+=end
           #acquisitionAuthorizer
-          acquisition_authorizer = attributes["acquisition_authorizer"]
+          acquisition_authorizer = attributes["acquisitionauthorizer"]
           CSXML::Helpers.add_person xml, 'acquisitionAuthorizer', acquisition_authorizer if acquisition_authorizer
-
-          #owner
-          CSXML.add_repeat xml, 'owners', [{
-            'owner' => CSURN.get_authority_urn('personauthorities', 'person', attributes['owner']),
-          }] if attributes['owner']
-
+          CSXML.add xml, 'acquisitionAuthorizerDate', CSDTP.parse(attributes['acquisitionauthorizerdate']).earliest_scalar rescue nil
+          #acquisitionDateGroupList
+          CSXML.add_group_list xml, 'acquisitionDate', [{
+           "dateDisplayDate" => attributes["acquisitiondategroup"],
+           "dateEarliestScalarValue" => CSDTP.parse(attributes["acquisitiondategroup"]).earliest_scalar,
+          }] rescue nil
+          #acquisitionFunding
+          funding_currency = CSURN.get_vocab_urn('currency', attributes['acquisitionfundingcurrency'])
+          funding_source = CSURN.get_authority_urn('orgauthorities', 'organization', attributes['acquisitionfundingcurrency'])
+          CSXML.add_list xml, 'acquisitionFunding', [{
+           'acquisitionFundingCurrency' => funding_currency,
+           'acquisitionFundingValue' => attributes['acquisitionfundingvalue'],
+           'acquisitionFundingSource' => funding_source,
+           'acquisitionFundingSourceProvisos' => attributes['acquisitionfundingsourceprovisos'],
+          }]
           #acquisitionMethod
-          CSXML.add xml, 'acquisition_method', attributes['acquisition_method']
-
+          CSXML.add xml, 'acquisitionMethod', attributes['acquisitionmethod']
           #acquisitionNote
-          CSXML.add xml, 'acquisitionNote', attributes["acquisition_note"]
-
+          CSXML.add xml, 'acquisitionNote', attributes["acquisitionnote"]
+          #acquisitionProvisos
+          CSXML.add xml, 'acquisitionProvisos', attributes["acquisitionprovisos"]
+          #acquisitionReason
+          CSXML.add xml, 'acquisitionReason', attributes["acquisitionreason"]
+          #owner
+          owners = []
+	  owners << { 'owner' => CSURN.get_authority_urn('personauthorities', 'person', attributes['ownerperson']) } if attributes['ownerperson']
+	  owners << { 'owner' => CSURN.get_authority_urn('orgauthorities', 'organization', attributes['ownerorganization']) } if attributes['ownerorganization']
+	  CSXML.add_repeat xml, 'owners', owners
+          #acquisitionSource
+          CSXML.add_repeat xml, 'acquisitionSources', [{'acquisitionSource' => attributes['acquisitionsource']}]
           #creditLine
-          CSXML.add xml, 'creditLine', attributes["credit_line"]
+          CSXML.add xml, 'creditLine', attributes["creditline"]
+          #groupPurchasePrice
+          currency = CSURN.get_vocab_urn('currency', attributes['grouppurchasepricecurrency'])
+          CSXML.add xml, 'groupPurchasePriceCurrency', currency
+          CSXML.add xml, 'groupPurchasePriceValue', attributes['grouppurchasepricevalue']
+          #objectOfferPrice
+          offer_currency = CSURN.get_vocab_urn('currency', attributes['objectofferpricecurrency'])
+          CSXML.add xml, 'objectOfferPriceCurrency', offer_currency
+          CSXML.add xml, 'objectOfferPriceValue', attributes['objectofferpricevalue']
+          #objectPurchaseOfferPrice
+          object_purchase_offer_currency = CSURN.get_vocab_urn('currency', attributes['objectpurchaseofferpricecurrency'])
+          CSXML.add xml, 'objectPurchaseOfferPriceCurrency', object_purchase_offer_currency
+          CSXML.add xml, 'objectPurchaseOfferPriceValue', attributes['objectpurchaseofferpricevalue']
+          #objectPurchasePrice
+          object_purchase_currency = CSURN.get_vocab_urn('currency', attributes['objectpurchasepricecurrency'])
+          CSXML.add xml, 'objectPurchasePriceCurrency', object_purchase_currency
+          CSXML.add xml, 'objectPurchasePriceValue', attributes['objectpurchasepricevalue']
+          #originalObjectPurchasePrice
+          original_object_currency = CSURN.get_vocab_urn('currency', attributes['originalobjectpurchasepricecurrency'])
+          CSXML.add xml, 'originalObjectPurchasePriceCurrency', original_object_currency
+          CSXML.add xml, 'originalObjectPurchasePriceValue', attributes['originalobjectpurchasepricevalue']
+          #transferOfTitleNumber
+          CSXML.add xml, 'transferOfTitleNumber', attributes['transferoftitlenumber']
+          #fieldCollectionEventName
+          CSXML.add_repeat xml, 'fieldCollectionEventNames', [{'fieldCollectionEventName' => attributes['fieldcollectioneventname']}]
         end
       end
     end
