af62d15535972215b1c0331a08190ee250adc27e
Paul Mulonzia <paulmwendwa43@gmail.com>
2019-11-05 15:15:37 +0000

    loan out converter updates

--- lib/collectionspace/converter/core/loanout.rb
+++ lib/collectionspace/converter/core/loanout.rb
@@ -10,16 +10,27 @@ module CollectionSpace
         end
 
         def self.map(xml, attributes)
-          CSXML.add xml, 'loanOutNumber', attributes["loan_out_number"]
+          CSXML.add xml, 'loanOutNumber', attributes["loanoutnumber"]
+=begin
           CSXML::Helpers.add_organization xml, 'borrower', attributes["borrower"]
-          CSXML::Helpers.add_person xml, 'borrowersAuthorizer', attributes["borrower's_authorizer"]
-          CSXML::Helpers.add_person xml, 'lendersAuthorizer', attributes["lender's_authorizer"]
+          CSXML::Helpers.add_person xml, 'borrowersAuthorizer', attributes["borrowersauthorizer"]
+          CSXML.add xml, 'borrowersAuthorizationDate', CSDTP.parse(attributes['borrowersauthorizationdate']).earliest_scalar
+          CSXML::Helpers.add_person xml, 'borrowersContact', attributes["borrowerscontact"]
+          CSXML::Helpers.add_person xml, 'lendersAuthorizer', attributes["lendersauthorizer"]
+          CSXML.add xml, 'lendersAuthorizationDate', CSDTP.parse(attributes["lendersauthorizationdate"]).earliest_scalar
+          CSXML::Helpers.add_person xml, 'lendersContact', attributes["lenderscontact"]
+          CSXML.add xml, 'loanOutDate', CSDTP.parse(attributes["loanoutdate"]).earliest_scalar
+          CSXML.add xml, 'loanReturnDate', CSDTP.parse(attributes["loanreturndate"]).earliest_scalar
+          CSXML.add xml, 'loanRenewalApplicationDate', CSDTP.parse(attributes["loanrenewalapplicationdate"]).earliest_scalar
+          CSXML.add xml, 'specialConditionsOfLoan', attributes["specialconditionsofloan"]
+          CSXML.add xml, 'loanOutNote', scrub_fields([attributes["loanoutnote"]])
+          CSXML.add xml, 'loanPurpose', attributes["loanpurpose"]
           CSXML.add_group_list xml, 'loanStatus', [{
-            "loanStatus" =>  CSXML::Helpers.get_vocab('loanoutstatus', attributes["loan_status"]),
-            "loanStatusDate" => attributes["loan_status_date"],
+            "loanStatus" =>  CSXML::Helpers.get_vocab('loanoutstatus', attributes["loanstatus"]),
+            "loanStatusDate" => CSDTP.parse(attributes["loanstatusdate"]).earliest_scalar,
+            "loanStatusNote" => attributes["loanstatusnote"]
           }]
-          CSXML.add xml, 'loanOutDate', attributes["loan_out_date"]
-          CSXML.add xml, 'loanOutNote', scrub_fields([attributes["loan_out_note"]])
+=end
         end
       end
     end
