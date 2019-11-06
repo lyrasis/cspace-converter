1772c2e24b3974f10753b4a98ebe794d9f232cf1
Mark Cooper <markchristophercooper@gmail.com>
2019-11-06 11:15:26 -0800

    Chipping away at core collectionobject

--- lib/collectionspace/converter/core/media.rb
+++ lib/collectionspace/converter/core/media.rb
@@ -9,49 +9,34 @@ module CollectionSpace
           end
         end
 
+        def self.pairs
+          {
+            'identificationnumber' => 'identificationNumber',
+            'title' => 'title',
+            'copyrightstatement' => 'copyrightStatement',
+            'coverage' => 'coverage',
+            'source' => 'source',
+            'sourceurl' => 'externalUrl',
+          }
+        end
+
         def self.map(xml, attributes)
-          CSXML.add xml, 'identificationNumber', attributes["identificationnumber"]
-          CSXML.add xml, 'title', attributes["title"]
+          CSXML::Helpers.add_pairs(xml, attributes, CoreMedia.pairs)
           CSXML::Helpers.add_person xml, 'contributor', attributes["contributor"] if attributes["contributortype"] == "person"
           CSXML::Helpers.add_organization xml, 'contributor', attributes["contributor"] if attributes["contributortype"] == "organization"
-          CSXML.add xml, 'copyrightStatement', attributes["copyrightstatement"]
-          CSXML.add xml, 'coverage', attributes["coverage"]
           CSXML::Helpers.add_person xml, 'creator', attributes["creator"] if attributes["creatortype"] == "person"
           CSXML::Helpers.add_organization xml, 'creator', attributes["creator"] if attributes["creatortype"] == "organization"
-          CSXML.add_group_list xml, 'date', [{
-            "dateDisplayDate" => CSDTP.parse(attributes["date"]).display_date,
-            "dateEarliestScalarValue" => CSDTP.parse(attributes["date"]).earliest_scalar,
-            "dateLatestScalarValue" => CSDTP.parse(attributes["date"]).latest_scalar,
-          }]
-          CSXML.add xml, 'description', scrub_fields([attributes["description"]])
-          # measuredPartGroupList
-          overall_data = {
-            "measuredPart" => attributes["measuredpart"],
-            "dimensionSummary" => attributes["dimensionsummary"],
-          }
-          dimensions = []
-          dims = split_mvf attributes, 'dimension'
-          values = split_mvf attributes, 'value'
-          unit = attributes["measurementunit"]
-          by = CSXML::Helpers.get_authority(
-            'personauthorities', 'person', attributes["measuredby"]
+          CSXML::Helpers.add_date_group_list(
+            xml, 'date', [CSDTP.parse(attributes['date'])]
           )
-          method = attributes["measurementmethod"]
-          date = CSDTP.parse(attributes["valuedate"]).earliest_scalar
-          qualifier = attributes["valuequalifier"]
-          note = attributes["dimensionnote"]
-          dims.each_with_index do |dim, index|
-            dimensions << { "dimension" => dim, "value" => values[index], "measurementUnit" => unit, "measuredBy" => by, "measurementMethod" => method, "valueDate" => date, "valueQualifier" => qualifier, "dimensionNote" => note}
-          end
-          CSXML.add_group_list xml, 'measuredPart', [overall_data], 'dimension', dimensions
+          CSXML.add xml, 'description', scrub_fields([attributes["description"]])
+          CSXML::Helpers.add_measured_part_group_list(xml, attributes)
           CSXML.add_repeat xml, 'language', [{'language' => CSXML::Helpers.get_vocab('languages', attributes["language"])}], 'List'
           CSXML::Helpers.add_person xml, 'publisher', attributes["publisher"] if attributes["publishertype"] == "person"
           CSXML::Helpers.add_organization xml, 'publisher', attributes["publisher"] if attributes["publishertype"] == "organization"
           CSXML.add_repeat xml, 'relation', [{'relation' => attributes['relation']}], 'List'
           CSXML::Helpers.add_person xml, 'rightsHolder', attributes["rightsholder"] if attributes["rightsholdertype"] == "person"
           CSXML::Helpers.add_organization xml, 'rightsHolder', attributes["rightsholder"] if attributes["rightsholdertype"] == "organization"
-          CSXML.add xml, 'source', attributes["source"]
-          CSXML.add xml, 'externalUrl', attributes["sourceurl"]
           CSXML.add_repeat xml, 'subject', [{'subject' => attributes['subject']}], 'List'
           CSXML.add_repeat xml, 'type', [{'type' => attributes['type']}], 'List'
         end
--- spec/collectionspace/tools/xml_helpers_spec.rb
+++ spec/collectionspace/tools/xml_helpers_spec.rb
@@ -9,6 +9,24 @@ RSpec.describe CSXML::Helpers do
   let(:attributes_title_only) {{
     'title' => 'El gato!'
   }}
+  let(:attributes_measured_part_group_list) {{
+    'dimensionsummary' => '8.5 x 11',
+    'measuredpart' => 'frame',
+    'dimension' => 'height; width',
+    'value' => '8.5; 11',
+    'measurementunit' => 'inches',
+    'measuredby' => 'Jurgen Klopp',
+    'measurementmethod' => 'ruler',
+    'valuedate' => '2019-12-01',
+    'valuequalifier' => 'qualifier text',
+    'dimensionnote' => 'note text',
+  }}
+  let(:attributes_pairs) {{
+    'letitbe' => 'Let it Be.'
+  }}
+  let(:attributes_simple_groups) {{
+    'simple' => 'This is a simple group value!'
+  }}
   let(:attributes_title_with_language) {{
     'title' => 'El gato!',
     'titlelanguage' => 'Spanish',
@@ -19,15 +37,50 @@ RSpec.describe CSXML::Helpers do
     'titletranslation' => 'Duck!',
     'titletranslationlanguage' => 'English',
   }}
+  let(:simple_groups) {{
+    'simple' => 'simple'
+  }}
+  let(:pairs) {{
+    'letitbe' => 'letItBe'
+  }}
   let(:person_name) { 'Jurgen Klopp' }
   let(:person_refname) { "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(JurgenKlopp1289035554)'Jurgen Klopp'" }
   let(:vocab) { 'English' }
   let(:vocab_refname) { "urn:cspace:core.collectionspace.org:vocabularies:name(languages):item:name(english)'English'" }
 
-  it "can 'add_person' correctly" do
+  xit "can 'add date group' correctly" do
+    # TODO
+  end
+
+  it "can 'add measured part group list' correctly" do
+    CSXML::Helpers.add_measured_part_group_list(xml, attributes_measured_part_group_list)
+    expect(doc(xml).xpath('/measuredPartGroupList/measuredPartGroup/measuredPart').text).to eq('frame')
+    expect(doc(xml).xpath('/measuredPartGroupList/measuredPartGroup/dimensionSummary').text).to eq('8.5 x 11')
+    expect(doc(xml).xpath('/measuredPartGroupList/measuredPartGroup/dimensionSubGroupList/dimensionSubGroup/measuredBy').text).to eq("#{person_refname}#{person_refname}")
+    expect(doc(xml).xpath('/measuredPartGroupList/measuredPartGroup/dimensionSubGroupList/dimensionSubGroup[1]/dimension').text).to eq('height')
+    expect(doc(xml).xpath('/measuredPartGroupList/measuredPartGroup/dimensionSubGroupList/dimensionSubGroup[1]/value').text).to eq('8.5')
+    expect(doc(xml).xpath('/measuredPartGroupList/measuredPartGroup/dimensionSubGroupList/dimensionSubGroup[2]/dimension').text).to eq('width')
+    expect(doc(xml).xpath('/measuredPartGroupList/measuredPartGroup/dimensionSubGroupList/dimensionSubGroup[2]/value').text).to eq('11')
+  end
+
+  it "can 'add pairs' correctly" do
+    CSXML::Helpers.add_pairs(xml, attributes_pairs, pairs)
+    expect(doc(xml).xpath('/letItBe').text).to eq('Let it Be.')
+  end
+
+  it "can 'add person' correctly" do
     expect(CSXML::Helpers.add_person(xml, 'owner', nil)).to be nil
     CSXML::Helpers.add_person(xml, 'owner', person_name)
     expect(doc(xml).xpath('/owner').text).to eq(person_refname)
+  end
+
+  it "can 'add simple groups' correctly" do
+    CSXML::Helpers.add_simple_groups(xml, attributes_simple_groups, simple_groups)
+    expect(doc(xml).xpath('/simpleGroupList/simpleGroup/simple').text).to eq('This is a simple group value!')
+  end
+
+  xit "can 'add simple repeats' correctly" do
+    # TODO
   end
 
   it "can 'add title' only correctly" do
@@ -58,5 +111,13 @@ RSpec.describe CSXML::Helpers do
   it "can 'get vocab' correctly" do
     expect(CSXML::Helpers.get_vocab('languages', nil)).to be nil
     expect(CSXML::Helpers.get_vocab('languages', 'English')).to eq(vocab_refname)
+  end
+
+  it "can identify reserved field names correctly" do
+    expect(CSXML::Helpers.reserved?('comment')).to be true
+  end
+
+  xit "can safe split correctly" do
+    # TODO
   end
 end
