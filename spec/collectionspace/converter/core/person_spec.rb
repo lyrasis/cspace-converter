74f482c82820af61cb5f07a85d53a491c239d3db
Paul Mulonzia <paulmwendwa43@gmail.com>
2019-11-08 13:48:42 +0000

    Adding person auth mapping spec

---
+++ spec/collectionspace/converter/core/person_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe CollectionSpace::Converter::Core::CorePerson do
+  let(:attributes) { get_attributes('core', 'authperson_core_all.csv') }
+  let(:coreperson) { CorePerson.new(attributes) }
+  let(:doc) { Nokogiri::XML(coreperson.convert, nil, 'UTF-8') }
+  let(:record) { get_fixture('core_person.xml') }
+  let(:xpaths) {[
+    '/document/*/personTermGroupList/personTermGroup/termDisplayName',
+    '/document/*/personTermGroupList/personTermGroup/termName',
+    '/document/*/personTermGroupList/personTermGroup/foreName',
+    '/document/*/personTermGroupList/personTermGroup/middleName',
+    '/document/*/personTermGroupList/personTermGroup/surName',
+    '/document/*/personTermGroupList/personTermGroup/initials',
+    '/document/*/personTermGroupList/personTermGroup/salutation',
+    '/document/*/personTermGroupList/personTermGroup/title',
+    '/document/*/personTermGroupList/personTermGroup/nameAdditions',
+    '/document/*/personTermGroupList/personTermGroup/termLanguage',
+    '/document/*/personTermGroupList/personTermGroup/termPrefForLang',
+    #'/document/*/personTermGroupList/personTermGroup/termType',
+    '/document/*/personTermGroupList/personTermGroup/termQualifier',
+    { xpath: '/document/*/personTermGroupList/personTermGroup/termSource', transform: ->(text) { CSURN.parse(text)[:label] } },
+    '/document/*/personTermGroupList/personTermGroup/termSourceID',
+    '/document/*/personTermGroupList/personTermGroup/termSourceDetail',
+    '/document/*/personTermGroupList/personTermGroup/termSourceNote',
+    '/document/*/personTermGroupList/personTermGroup/termStatus',
+    # '/document/*/birthDateGroup/dateDisplayDate',
+    # '/document/*/deathDateGroup/dateDisplayDate',
+    '/document/*/birthPlace',
+    '/document/*/deathPlace',
+    '/document/*/groups/group',
+    '/document/*/nationalities/nationality',
+    '/document/*/gender',
+    '/document/*/occupations/occupation',
+    '/document/*/schoolsOrStyles/schoolOrStyle',
+    '/document/*/bioNote',
+    '/document/*/nameNote',
+    '/document/*/emailGroupList/emailGroup/email',
+    '/document/*/emailGroupList/emailGroup/emailType',
+    '/document/*/telephoneNumberGroupList/telephoneNumberGroup/telephoneNumber',
+    '/document/*/telephoneNumberGroupList/telephoneNumberGroup/telephoneNumberType',
+    '/document/*/faxNumberGroupList/faxNumberGroup/faxNumber',
+    '/document/*/faxNumberGroupList/faxNumberGroup/faxNumberType',
+    '/document/*/webAddressGroupList/webAddressGroup/webAddress',
+    '/document/*/webAddressGroupList/webAddressGroup/webAddressType',
+    '/document/*/addressGroupList/addressGroup/addressType',
+    '/document/*/addressGroupList/addressGroup/addressPlace1',
+    '/document/*/addressGroupList/addressGroup/addressPlace2',
+    '/document/*/addressGroupList/addressGroup/addressMunicipality',
+    '/document/*/addressGroupList/addressGroup/addressStateOrProvince',
+    '/document/*/addressGroupList/addressGroup/addressPostCode',
+    '/document/*/addressGroupList/addressGroup/addressCounty',
+  ]}
+
+  it "Maps attributes correctly" do
+    test_converter(doc, record, xpaths)
+  end
+end
+
+
