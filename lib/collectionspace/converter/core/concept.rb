f2d98a045db59891a589ffbc17ef57a7ce28c954
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2019-11-15 10:49:06 -0500

    initial work, files

--- lib/collectionspace/converter/core/concept.rb
+++ lib/collectionspace/converter/core/concept.rb
@@ -10,7 +10,48 @@ module CollectionSpace
         end
 
         def self.map(xml, attributes)
-          # TODO
+
+          # conceptTermGroupList
+          concept_term_by_attr = {
+            'termsourceid' => { :xmlfield => 'termSourceID' },
+            'termsourcedetail' => { :xmlfield => 'termSourceDetail' },
+            'termsourcenote' => { :xmlfield => 'termSourceNote' },
+            'termformatteddisplayname' => { :xmlfield => 'termFormattedDisplayName' },
+            'historicalstatus' => { :xmlfield => 'historicalStatus' },
+            'termtype' => { :xmlfield => 'termType' },
+            'termsource' => { :xmlfield => 'termSource' },
+            'termstatus' => { :xmlfield => 'termStatus' },
+            'termlanguage' => { :xmlfield => 'termLanguage' },
+            'termname' => { :xmlfield => 'termName' },
+            'termqualifier' => { :xmlfield => 'termQualifier' },
+            'termprefforlang' => { :xmlfield => 'termPrefForLang' },
+            'termdisplayname' => { :xmlfield => 'termDisplayName' },
+            'termflag' => { :xmlfield => 'termFlag' }
+          }
+
+          tdata_by_attr = concept_term_by_attr.map { |attr, h | h[:vals] = split_mvf attributes, attr }
+          #tdata_split = tdata_by_attr.each{ |attr, h| h[:svals] = h[
+          
+
+                    
+          
+          #CSXML.add_group_list xml, 'conceptTerm', [term_data]
+          
+          # materials = split_mvf attributes, 'material'
+          # materials.each do |m|
+          #   mgs << { "material" => m }
+          # end
+          # CSXML.add_group_list xml, 'material', mgs
+
+          # CSXML.add xml, 'termDisplayName', attributes['termDisplayName']
+
+          # CSXML.add_repeat xml, 'conceptRecordTypes', [{
+          #   'conceptRecordType' => CSURN.get_vocab_urn('concepttype', attributes['conceptrecordtype'])
+          # }] if attributes['conceptrecordtype']
+          
+          # CSXML.add xml, 'scopeNote', attributes['scopenote']
+          # #termSourceID
+          # #termSourceNote
         end
       end
     end
--- spec/collectionspace/converter/core/concept_spec.rb
+++ spec/collectionspace/converter/core/concept_spec.rb
@@ -4,10 +4,30 @@ RSpec.describe CollectionSpace::Converter::Core::CoreConcept do
   let(:attributes) { get_attributes('core', 'authconcept_nomenclature_terms.csv') }
   let(:coreconcept) { CoreConcept.new(attributes) }
   let(:doc) { Nokogiri::XML(coreconcept.convert, nil, 'UTF-8') }
-  # let(:record) { get_fixture('core_concept.xml') }
-  let(:xpaths) {[]}
+  let(:record) { get_fixture('core_concept.xml') }
+  let(:xpaths) {[
+    '/document/*/conceptRecordTypes/conceptRecordType',
+    '/document/*/conceptTermGroupList/conceptTermGroup/termDisplayName',
+    '/document/*/scopeNote'
+  ]}
 
-  xit "Maps attributes correctly" do
+  it "Maps attributes correctly" do
+    test_converter(doc, record, xpaths)
+  end
+end
+
+RSpec.describe CollectionSpace::Converter::Core::CoreConcept do
+  let(:attributes) { get_attributes('core', 'authconcept_multival_terms.csv') }
+  let(:coreconcept) { CoreConcept.new(attributes) }
+  let(:doc) { Nokogiri::XML(coreconcept.convert, nil, 'UTF-8') }
+  let(:record) { get_fixture('core_concept_multival.xml') }
+  let(:xpaths) {[
+    '/document/*/conceptRecordTypes/conceptRecordType',
+    '/document/*/conceptTermGroupList/conceptTermGroup/termDisplayName',
+    '/document/*/scopeNote'
+  ]}
+
+  it "Maps attributes correctly" do
     test_converter(doc, record, xpaths)
   end
 end
