237d3ae301f289d4c5aae59719e2960a2b76a542
Mark Cooper <markchristophercooper@gmail.com>
2019-11-19 20:02:20 -0800

    Clear out old mappings [ci skip]

--- lib/collectionspace/converter/core/conditioncheck.rb
+++ lib/collectionspace/converter/core/conditioncheck.rb
@@ -10,14 +10,7 @@ module CollectionSpace
         end
 
         def self.map(xml, attributes)
-          CSXML.add xml, 'conditionCheckRefNumber', attributes.fetch("condition_check_reference_number")
-          CSXML.add_group_list xml, 'conditionCheck', [{
-            'conditionDate' => attributes['condition date'],
-            #last gsub is a hack.  We should probably use the vocab API service
-            'condition' => attributes['condition'].downcase.gsub(' ', '_').gsub('/', '_').gsub(/_+/,'_').gsub('not_ex','notex')
-          }] if attributes['condition']
-          CSXML::Helpers.add_person xml, 'conditionChecker', attributes["condition_checker"]
-          CSXML.add xml, 'conditionCheckNote', attributes.fetch("condition_check_note")
+          # TODO
         end
       end
     end
