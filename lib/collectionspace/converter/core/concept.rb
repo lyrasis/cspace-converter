7e64ecf95f9ea951d20b4dbf2653532c15df0c77
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2019-11-20 15:04:12 -0500

    core concept conversion

--- lib/collectionspace/converter/core/concept.rb
+++ lib/collectionspace/converter/core/concept.rb
@@ -12,46 +12,20 @@ module CollectionSpace
         def self.map(xml, attributes)
 
           # conceptTermGroupList
-          concept_term_by_attr = {
-            'termsourceid' => { :xmlfield => 'termSourceID' },
-            'termsourcedetail' => { :xmlfield => 'termSourceDetail' },
-            'termsourcenote' => { :xmlfield => 'termSourceNote' },
-            'termformatteddisplayname' => { :xmlfield => 'termFormattedDisplayName' },
-            'historicalstatus' => { :xmlfield => 'historicalStatus' },
-            'termtype' => { :xmlfield => 'termType' },
-            'termsource' => { :xmlfield => 'termSource' },
-            'termstatus' => { :xmlfield => 'termStatus' },
-            'termlanguage' => { :xmlfield => 'termLanguage' },
-            'termname' => { :xmlfield => 'termName' },
-            'termqualifier' => { :xmlfield => 'termQualifier' },
-            'termprefforlang' => { :xmlfield => 'termPrefForLang' },
-            'termdisplayname' => { :xmlfield => 'termDisplayName' },
-            'termflag' => { :xmlfield => 'termFlag' }
-          }
+           term_data = {
+             'termSourceID' => attributes['termsourceid'],
+             'termSourceNote' => attributes['termsourcenote'],
+             'termDisplayName' => attributes['termdisplayname']
+           }
+          CSXML.add_group_list xml, 'conceptTerm', [term_data]
 
-          tdata_by_attr = concept_term_by_attr.map { |attr, h | h[:vals] = split_mvf attributes, attr }
-          #tdata_split = tdata_by_attr.each{ |attr, h| h[:svals] = h[
-          
+          CSXML.add xml, 'termDisplayName', attributes['termDisplayName']
 
-                    
-          
-          #CSXML.add_group_list xml, 'conceptTerm', [term_data]
-          
-          # materials = split_mvf attributes, 'material'
-          # materials.each do |m|
-          #   mgs << { "material" => m }
-          # end
-          # CSXML.add_group_list xml, 'material', mgs
-
-          # CSXML.add xml, 'termDisplayName', attributes['termDisplayName']
-
-          # CSXML.add_repeat xml, 'conceptRecordTypes', [{
-          #   'conceptRecordType' => CSURN.get_vocab_urn('concepttype', attributes['conceptrecordtype'])
-          # }] if attributes['conceptrecordtype']
-          
-          # CSXML.add xml, 'scopeNote', attributes['scopenote']
-          # #termSourceID
-          # #termSourceNote
+          CSXML.add_repeat xml, 'conceptRecordTypes', [{
+            'conceptRecordType' => CSURN.get_vocab_urn('concepttype', attributes['conceptrecordtype'])
+          }] if attributes['conceptrecordtype']
+         
+          CSXML.add xml, 'scopeNote', attributes['scopenote']
         end
       end
     end
--- spec/collectionspace/converter/core/concept_spec.rb
+++ spec/collectionspace/converter/core/concept_spec.rb
@@ -7,22 +7,8 @@ RSpec.describe CollectionSpace::Converter::Core::CoreConcept do
   let(:record) { get_fixture('core_concept.xml') }
   let(:xpaths) {[
     '/document/*/conceptRecordTypes/conceptRecordType',
-    '/document/*/conceptTermGroupList/conceptTermGroup/termDisplayName',
-    '/document/*/scopeNote'
-  ]}
-
-  it "Maps attributes correctly" do
-    test_converter(doc, record, xpaths)
-  end
-end
-
-RSpec.describe CollectionSpace::Converter::Core::CoreConcept do
-  let(:attributes) { get_attributes('core', 'authconcept_multival_terms.csv') }
-  let(:coreconcept) { CoreConcept.new(attributes) }
-  let(:doc) { Nokogiri::XML(coreconcept.convert, nil, 'UTF-8') }
-  let(:record) { get_fixture('core_concept_multival.xml') }
-  let(:xpaths) {[
-    '/document/*/conceptRecordTypes/conceptRecordType',
+    '/document/*/conceptTermGroupList/conceptTermGroup/termSourceID',
+    '/document/*/conceptTermGroupList/conceptTermGroup/termSourceNote',
     '/document/*/conceptTermGroupList/conceptTermGroup/termDisplayName',
     '/document/*/scopeNote'
   ]}
