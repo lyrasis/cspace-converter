d471d4d38d9b1bd2e0e2e90d26ca2682e1ef08ac
Paul Mulonzia <paulmwendwa43@gmail.com>
2019-12-10 22:38:26 +0000

    additional field mapping

--- lib/collectionspace/converter/anthro/osteology.rb
+++ lib/collectionspace/converter/anthro/osteology.rb
@@ -28,7 +28,34 @@ module CollectionSpace
         end
 
         def self.extension(xml, attributes)
-          # TODO
+          CSXML.add xml, 'Notes_DentalPathology', attributes['notes_dentalpathology']
+          CSXML.add xml, 'Notes_CranialPathology', attributes['notes_cranialpathology']
+          CSXML.add xml, 'Notes_PostcranialPathology', attributes['notes_postcranialpathology']
+          CSXML.add xml, 'Notes_CulturalModifications', attributes['notes_culturalmodifications']
+          CSXML.add xml, 'Notes_NHTaphonomicAlterations', attributes['notes_nhtaphonomicalterations']
+          CSXML.add xml, 'Notes_CuratorialSuffixing', attributes['notes_curatorialsuffixing']
+          CSXML.add xml, 'cranialDeformationPresent', attributes['cranialdeformationpresent']
+          cranial_deform = []
+          deform_cat = CSDR.split_mvf attributes, 'cranialdeformationcategory'
+          deform_cat.each_with_index do |dfct, index|
+            cranial_deform << {"cranialDeformationCategory" => CSXML::Helpers.get_vocab('cranialdeformationcategory', dfct)}
+          end
+          CSXML.add_repeat xml, 'cranialDeformationCategories', cranial_deform
+          CSXML.add xml, 'cranialDeformationNote', attributes['cranialdeformationnote']
+          CSXML.add xml, 'trepanationPresent', attributes['trepanationpresent']
+          overall_trepanation = []
+          trepanation_location = CSDR.split_mvf attributes, 'trepanationlocation'
+          trepanation_certainty = CSDR.split_mvf attributes, 'trepanationcertainty'
+          dim_max = CSDR.split_mvf attributes, 'trepanationdimensionmax'
+          dim_min = CSDR.split_mvf attributes, 'trepanationdimensionmin'
+          trepanation_technique = CSDR.split_mvf attributes, 'trepanationtechnique'
+          trepanation_healing = CSDR.split_mvf attributes, 'trepanationhealing'
+          trepanation_note = CSDR.split_mvf attributes, 'trepanationnote'
+          trepanation_location.each_with_index do |trplc, index|
+            overall_trepanation << {"trepanationLocation" => trplc, "trepanationCertainty" => CSXML::Helpers.get_vocab('trepanationcertainty', trepanation_certainty[index]), "trepanationDimensionMax" => dim_max[index], "trepanationDimensionMin" => dim_min[index], "trepanationTechnique" => CSXML::Helpers.get_vocab('trepanationtechnique', trepanation_technique[index]), "trepanationHealing" => CSXML::Helpers.get_vocab('trepanationhealing', trepanation_healing[index]), "trepanationNote" => trepanation_note[index]}
+          end
+          CSXML.add_group_list xml, 'trepanation', overall_trepanation
+          CSXML.add xml, 'trepanationGeneralNote', attributes['trepanationgeneralnote']
         end
 
         def self.map(xml, attributes)
@@ -52,6 +79,20 @@ module CollectionSpace
             sexdetermination << {"sexDetermination" => sxd, "sexDeterminationAnalyst" => CSXML::Helpers.get_authority('personauthorities', 'person', determination_analyst[index]), "sexDeterminationNote" => determination_note[index]}
           end 
           CSXML.add_group_list xml, 'sexDetermination', sexdetermination
+          CSXML.add xml, 'completeness', CSXML::Helpers.get_vocab('osteocompleteness',  attributes['completeness'])
+          CSXML.add xml, 'completenessNote', attributes['completenessnote']
+          CSXML.add xml, 'molarsPresent', attributes['molarspresent']
+          CSXML.add xml, 'dentitionScore', CSXML::Helpers.get_vocab('dentitionscore',  attributes['dentitionscore'])
+          CSXML.add xml, 'dentitionNote', attributes['dentitionnote']
+          CSXML.add xml, 'mortuaryTreatment', CSXML::Helpers.get_vocab('mortuarytreatment',  attributes['mortuarytreatment'])
+          CSXML.add xml, 'mortuaryTreatmentNote', attributes['mortuarytreatmentnote']
+          CSXML.add xml, 'behrensmeyerSingleLower', CSXML::Helpers.get_vocab('behrensmeyer',  attributes['behrensmeyersinglelower'])
+          CSXML.add xml, 'behrensmeyerUpper', CSXML::Helpers.get_vocab('behrensmeyer',  attributes['behrensmeyerupper'])
+          CSXML.add xml, 'NotesOnElementInventory', attributes['notesonelementinventory']
+          CSXML.add xml, 'pathologyNote', attributes['pathologynote']
+          CSXML.add xml, 'InventoryIsComplete', attributes['inventoryiscomplete']
+          CSXML.add xml, 'inventoryAnalyst', CSXML::Helpers.get_authority('personauthorities', 'person',  attributes['inventoryanalyst'])
+          CSXML.add xml, 'inventoryDate', CSDTP.parse(attributes['inventorydate']).earliest_scalar
         end
       end
     end
