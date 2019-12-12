b3a695ac848dc38e2695203bbd310edf674f4f4b
Paul Mulonzia <paulmwendwa43@gmail.com>
2019-12-12 20:46:51 +0000

    adding new fields

--- lib/collectionspace/converter/core/collectionobject.rb
+++ lib/collectionspace/converter/core/collectionobject.rb
@@ -18,7 +18,10 @@ module CollectionSpace
             'copynumber' => 'copyNumber',
             'editionnumber' => 'editionNumber',
             'phase' => 'phase',
-            'sex' => 'sex'
+            'sex' => 'sex',
+            'productionnote' => 'objectProductionNote',
+            'fieldcollectionnote' => 'fieldCollectionNote',
+            'fieldcollectionfeature' => 'fieldCollectionFeature'
           }
         end
 
@@ -89,33 +92,51 @@ module CollectionSpace
           }]
 
           # not simple because 'objectProductionPeople' singularized as 'objectProductionPerson'
-          CSXML.add_group_list xml, 'objectProductionPeople', [{"objectProductionPeople" => attributes["productionpeople"]}]
-
+          opp = []
+          productionpeople = CSDR.split_mvf attributes, 'productionpeople'
+          peoplerole = CSDR.split_mvf attributes, 'productionpeoplerole'
+          productionpeople.each_with_index do |prodppl, index|
+            opp << {"objectProductionPeople" => prodppl, "objectProductionPeopleRole" => peoplerole[index]}
+          end
+          CSXML.add_group_list xml, 'objectProductionPeople', opp
           CSXML.add_group_list xml, "objectComponent", [{
             "objectComponentName" => attributes["objectcomponentname"]
           }]
-
-          CSXML.add_list xml, 'objectName', [{
-            "objectName" => attributes["objectname"],
-          }], 'Group'
-
+          object_name = []
+          objectname = CSDR.split_mvf attributes, 'objectname'
+          objectname.each_with_index do |obj, index|
+            object_name << {"objectName" => obj}
+          end
+          CSXML.add_list xml, 'objectName', object_name, 'Group'
           CSXML.add_repeat xml, 'objectStatus', [{
             "objectStatus" => attributes["objectstatus"]
           }], 'List'
-          CSXML.add_list xml, 'otherNumber', [{
-            "numberValue" => attributes["numbervalue"],
-            "numberType" => attributes["numbertype"],
-          }]
-          CSXML.add_repeat xml, 'inventoryStatus', [{
-            "inventoryStatus" => CSXML::Helpers.get_vocab('inventorystatus', attributes["inventorystatus"])
-          }], 'List'
+          other_number = []
+          numbervalue = CSDR.split_mvf attributes, 'numbervalue'
+          numbertype = CSDR.split_mvf attributes, 'numbertype'
+          numbervalue.each_with_index do |numval, index|
+            other_number << {"numberValue" => numval, "numberType" => numbertype[index]}
+          end
+          CSXML.add_list xml, 'otherNumber', other_number
+          inventory_status = []
+          inventstat = CSDR.split_mvf attributes, 'inventorystatus'
+          inventstat.each_with_index do |inv, index|
+            inventory_status << {"inventoryStatus" => CSXML::Helpers.get_vocab('inventorystatus', inv)}
+          end
+          CSXML.add_repeat xml, 'inventoryStatus', inventory_status, 'List'
           pbt = []
           publish = CSDR.split_mvf attributes, 'publishto'
             publish.each_with_index do |pb, index|
               pbt << {"publishTo" =>  CSXML::Helpers.get_vocab('publishto', pb)}
           end
           CSXML.add_repeat xml, 'publishTo', pbt, 'List'
-
+          assoc_people = []
+          assocpeople = CSDR.split_mvf attributes, 'assocpeople'
+          assocpeopletype = CSDR.split_mvf attributes, 'assocpeopletype'
+            assocpeople.each_with_index do |assc, index|
+              assoc_people << {"assocPeople" =>  assc, "assocPeopleType" =>  assocpeopletype[index]}
+          end
+          CSXML.add_group_list xml, 'assocPeople', assoc_people
         end
       end
     end
