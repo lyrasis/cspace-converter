bca9764bf1c8c709a225f85444d277ad03b83088
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-01-14 17:18:17 +0000

    adding new fields

--- lib/collectionspace/converter/core/exhibition.rb
+++ lib/collectionspace/converter/core/exhibition.rb
@@ -9,8 +9,48 @@ module CollectionSpace
           end
         end
 
+        def self.pairs
+          {
+            'exhibitionnumber' => 'exhibitionNumber',
+            'title' => 'title',
+            'type' => 'type',
+          }
+        end
+
+        def self.simple_groups
+          {
+          }
+        end
+
+        def self.simple_repeats
+          {
+          }
+        end
+
+        def self.simple_repeat_lists
+          {
+          }
+        end
+
         def self.map(xml, attributes)
-          # TODO
+          CSXML::Helpers.add_pairs(xml, attributes, CoreExhibition.pairs)
+=begin
+          CSXML::Helpers.add_simple_groups(xml, attributes, CoreExhibition.simple_groups)
+          CSXML::Helpers.add_simple_repeats(xml, attributes, CoreExhibition.simple_repeats)
+          CSXML::Helpers.add_simple_repeats(xml, attributes, CoreExhibition.simple_repeat_lists, 'List')
+=end
+          #CSXML.add xml, 'type', CSXML::Helpers.get_vocab('exhibitiontype', attributes['type'])
+          overallvenue = {
+            'venueorganization' => 'venue',
+            'venueopeningdate' => 'venueOpeningDate'
+          }
+          CSXML.add_group_list(
+            xml,
+            attributes,
+            'venue',
+            overallvenue,
+            group_suffix: 'Group'
+          )
         end
       end
     end
