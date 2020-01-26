15fe4fb64de3af4b78f2e9dbe17c654db2ec0d96
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-01-26 15:57:24 +0000

    adding new fields

--- lib/collectionspace/converter/core/exhibition.rb
+++ lib/collectionspace/converter/core/exhibition.rb
@@ -9,8 +9,158 @@ module CollectionSpace
           end
         end
 
+        def self.pairs
+          {
+            'exhibitionnumber' => 'exhibitionNumber',
+            'title' => 'title',
+            'type' => 'type',
+            'planningnote' => 'planningNote',
+            'curatorialnote' => 'curatorialNote',
+            'generalnote' => 'generalNote',
+            'boilerplatetext' => 'boilerplateText',
+            'galleryrotationname' => 'galleryRotationName',
+            'galleryrotationnote' => 'galleryRotationNote',
+            
+          }
+        end
+
+        def self.repeats
+          {
+            'organizerorganization' => ['organizers', 'organizer'],
+            'organizerperson' => ['organizers', 'organizer'],
+            'sponsororganization' => ['sponsors', 'sponsor'],
+            'sponsorperson' => ['sponsors', 'sponsor']
+
+          }
+        end
+
         def self.map(xml, attributes)
-          # TODO
+          CSXML::Helpers.add_pairs(xml, attributes, CoreExhibition.pairs,
+          pairstransforms = {
+            'type' => {'vocab' => 'exhibitiontype'},
+          })
+          CSXML::Helpers.add_repeats(xml, attributes, CoreExhibition.repeats,
+          repeatstransforms = {
+            'organizerorganization' => {'authority' => ['orgauthorities', 'organization']},
+            'organizerperson' => {'authority' => ['personauthorities', 'person']},
+            'sponsororganization' => {'authority' => ['orgauthorities', 'organization']},
+            'sponsorperson' => {'authority' => ['personauthorities', 'person']}
+          })
+          CSXML::Helpers.add_date_group(xml, 'galleryRotationStartDate', CSDTP.parse(attributes['galleryrotationstartdategroup']))
+          CSXML::Helpers.add_date_group(xml, 'galleryRotationEndDate', CSDTP.parse(attributes['galleryrotationenddategroup']))
+
+          venuedata = {
+            'venueorganization' => 'venue',
+            'venueplace' => 'venue',
+            'venuestoragelocation' => 'venue',
+            'venueopeningdate' => 'venueOpeningDate',
+            'venueclosingdate' => 'venueClosingDate',
+            'venueattendance' => 'venueAttendance',
+            'venueurl' => 'venueUrl'
+          }
+
+          venuetransforms = {
+            'venueorganization' => {'authority' => ['orgauthorities', 'organization']},
+            'venueplace' => {'authority' => ['placeauthorities', 'place']},
+            'venuestoragelocation' => {'authority' => ['locationauthorities', 'location']},
+            'venueopeningdate' => {'special' => 'unstructured_date_stamp'},
+            'venueclosingdate' => {'special' => 'unstructured_date_stamp'},
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'venue',
+            venuedata,
+            venuetransforms
+          )
+
+          workingdata = {
+            'workinggroupnote' => 'workingGroupNote',
+            'workinggrouptitle' => 'workingGroupTitle'
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'working',
+            workingdata
+          )
+          exhibitionpersondata = {
+            'exhibitionpersonrole' => 'exhibitionPersonRole',
+            'exhibitionpersonorganization' => 'exhibitionPerson',
+            'exhibitionpersonperson' => 'exhibitionPerson'
+          }
+          eptransforms = {
+            'exhibitionpersonorganization' => {'authority' => ['orgauthorities', 'organization']},
+            'exhibitionpersonperson' => {'authority' => ['personauthorities', 'person']},
+            'exhibitionpersonrole' => {'vocab' => 'exhibitionpersonrole'}
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'exhibitionPerson',
+            exhibitionpersondata,
+            eptransforms
+          )
+          exhibitionreferencedata = {
+            'exhibitionreference' => 'exhibitionReference',
+            'exhibitionreferencenote' => 'exhibitionReferenceNote',
+            'exhibitionreferencetype' => 'exhibitionReferenceType'
+          }
+          ertransforms = {
+            'exhibitionreference' => {'authority' => ['citationauthorities', 'citation']},
+            'exhibitionreferencetype' => {'vocab' => 'exhibitionreferencetype'}
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'exhibitionReference',
+            exhibitionreferencedata,
+            ertransforms
+          )
+          exhibitionsectiondata = {
+            'exhibitionsectionname' => 'exhibitionSectionName',
+            'exhibitionsectionobjects' => 'exhibitionSectionObjects',
+            'exhibitionsectionlocation' => 'exhibitionSectionLocation',
+            'exhibitionsectionnote' => 'exhibitionSectionNote',
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'exhibitionSection',
+            exhibitionsectiondata,
+          )
+          exhibitionstatusdata = {
+            'exhibitionstatus' => 'exhibitionStatus',
+            'exhibitionstatusdate' => 'exhibitionStatusDate',
+            'exhibitionstatusnote' => 'exhibitionStatusNote'
+          }
+          exhibitionstatustransforms = {
+            'exhibitionstatus' => {'vocab' => 'exhibitionstatus'},
+            'exhibitionstatusdate' => {'special' => 'unstructured_date_stamp'}
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'exhibitionStatus',
+            exhibitionstatusdata,
+            exhibitionstatustransforms
+          )
+          exhibitionobjectdata = {
+            'exhibitionobjectnumber' => 'exhibitionObjectNumber',
+            'exhibitionobjectname' => 'exhibitionObjectName',
+            'exhibitionobjectconstreatment' => 'exhibitionObjectConsTreatment',
+            'exhibitionobjectsection' => 'exhibitionObjectSection',
+            'exhibitionobjectcase' => 'exhibitionObjectCase',
+            'exhibitionobjectconscheckdate' => 'exhibitionObjectConsCheckDate',
+            'exhibitionobjectmount' => 'exhibitionObjectMount',
+            'exhibitionobjectseqnum' => 'exhibitionObjectSeqNum',
+            'exhibitionobjectnote' => 'exhibitionObjectNote',
+            'exhibitionobjectrotation' => 'exhibitionObjectRotation',
+
+          }
+          exhibitionobjecttransforms = {
+            'exhibitionobjectconscheckdate' => {'special' => 'unstructured_date_stamp'}
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'exhibitionObject',
+            exhibitionobjectdata,
+            exhibitionobjecttransforms
+          )
         end
       end
     end
