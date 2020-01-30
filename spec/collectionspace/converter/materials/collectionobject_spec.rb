0690aa073a0f505dca4c85bb97833db22fd650da
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-01-30 08:53:02 +0000

    updating minimally populated record

--- spec/collectionspace/converter/materials/collectionobject_spec.rb
+++ spec/collectionspace/converter/materials/collectionobject_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe CollectionSpace::Converter::Materials::MaterialsCollectionObject 
   before(:all) do
     ENV['CSPACE_CONVERTER_DOMAIN'] = 'materials.collectionspace.org'
   end
-
   let(:attributes) { get_attributes('materials', 'cataloging_materials_all.csv') }
   let(:materialscollectionobject) { MaterialsCollectionObject.new(attributes) }
   let(:doc) { get_doc(materialscollectionobject) }
@@ -27,7 +26,24 @@ RSpec.describe CollectionSpace::Converter::Materials::MaterialsCollectionObject 
     { xpath: "/document/#{ext}/materialGenericColors/materialGenericColor", transform: ->(text) { CSURN.parse(text)[:label].downcase }},
   ]}
 
+  context 'sample data row 1' do
+  it "Maps attributes correctly" do
+    test_converter(doc, record, xpaths)
+ end
+ end 
+  context 'sample data row 22' do
+      let(:attributes) { get_attributes_by_row('materials', 'cataloging_materials_all.csv', 22) }
+      let(:materialscollectionobject) { MaterialsCollectionObject.new(attributes) }
+      let(:doc) { get_doc(materialscollectionobject) }
+      let(:record) { get_fixture('materials_collectionobject_row22.xml') }
+      let(:p) { 'collectionobjects_common' }
+      let(:ext) { 'collectionobjects_materials' }
+      let(:xpaths) {[
+        "/document/#{p}/objectNumber",
+      ]}
+
   it "Maps attributes correctly" do
     test_converter(doc, record, xpaths)
   end
+ end
 end
