415225a25df3c75412580b936ef49845ca1b17e9
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-02-03 13:10:43 +0000

    testing minimally and maximally populated records

--- spec/collectionspace/converter/core/acquisition_spec.rb
+++ spec/collectionspace/converter/core/acquisition_spec.rb
@@ -3,19 +3,22 @@ require 'rails_helper'
 RSpec.describe CollectionSpace::Converter::Core::CoreAcquisition do
   let(:attributes) { get_attributes('core', 'acquisition_core_all.csv') }
   let(:coreacquisition) { CoreAcquisition.new(attributes) }
-  let(:doc) { Nokogiri::XML(coreacquisition.convert, nil, 'UTF-8') }
-  let(:record) { get_fixture('core_acquisition.xml') }
+  let(:doc) { get_doc(coreacquisition) }
+  let(:record) { get_fixture('core_acquisition_row1.xml') }
   let(:xpaths) {[
     { xpath: '/document/*/acquisitionAuthorizer', transform: ->(text) { CSURN.parse(text)[:label] } },
     '/document/*/acquisitionReferenceNumber',
     '/document/*/acquisitionAuthorizerDate',
+    '/document/*/accessionDateGroup/scalarValuesComputed',
     '/document/*/accessionDateGroup/dateDisplayDate',
-    '/document/*/accessionDateGroup/dateEarliestScalarValue',
     '/document/*/accessionDateGroup/dateLatestScalarValue',
+    '/document/*/accessionDateGroup/dateEarliestSingleDay',
+    '/document/*/accessionDateGroup/dateEarliestSingleYear',
+    { xpath: '/document/*/accessionDateGroup/dateEarliestSingleEra', transform: ->(text) { CSURN.parse(text)[:label] } },
+    '/document/*/accessionDateGroup/dateEarliestScalarValue',
+    '/document/*/acquisitionDateGroupList/acquisitionDateGroup/scalarValuesComputed',
     '/document/*/acquisitionDateGroupList/acquisitionDateGroup/dateDisplayDate',
-    '/document/*/acquisitionDateGroupList/acquisitionDateGroup/dateEarliestScalarValue',
-    '/document/*/acquisitionDateGroupList/acquisitionDateGroup/dateLatestScalarValue',
-    { xpath: '/document/*/acquisitionFundingList/acquisitionFunding/acquisitionFundingCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
+    #{ xpath: '/document/*/acquisitionFundingList/acquisitionFunding/acquisitionFundingCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
     '/document/*/acquisitionFundingList/acquisitionFunding/acquisitionFundingValue',
     { xpath: '/document/*/acquisitionFundingList/acquisitionFunding/acquisitionFundingSource', transform: ->(text) { CSURN.parse(text)[:label] } },
     '/document/*/acquisitionFundingList/acquisitionFunding/acquisitionFundingSourceProvisos',
@@ -23,24 +26,44 @@ RSpec.describe CollectionSpace::Converter::Core::CoreAcquisition do
     '/document/*/acquisitionNote',
     '/document/*/acquisitionProvisos',
     '/document/*/acquisitionReason',
-    { xpath: '/document/*/owners/owner', transform: ->(text) { CSURN.parse(text)[:label] } },
+    #{ xpath: '/document/*/owners/owner', transform: ->(text) { CSURN.parse(text)[:label] } },
     { xpath: '/document/*/acquisitionSources/acquisitionSource', transform: ->(text) { CSURN.parse(text)[:label] } },
     '/document/*/creditLine',
     { xpath: '/document/*/groupPurchasePriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
     '/document/*/groupPurchasePriceValue',
-    # { xpath: '/document/*/objectOfferPriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
-    # '/document/*/objectOfferPriceValue',
-    # { xpath: '/document/*/objectPurchaseOfferPriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
-    # '/document/*/objectPurchaseOfferPriceValue',
-    # { xpath: '/document/*/objectPurchasePriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
-    # '/document/*/objectPurchasePriceValue',
-    # { xpath: '/document/*/originalObjectPurchasePriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
-    # '/document/*/originalObjectPurchasePriceValue',
-    # '/document/*/transferOfTitleNumber',
+    { xpath: '/document/*/objectOfferPriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
+    '/document/*/objectOfferPriceValue',
+    { xpath: '/document/*/objectPurchaseOfferPriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
+    '/document/*/objectPurchaseOfferPriceValue',
+    { xpath: '/document/*/objectPurchasePriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
+    '/document/*/objectPurchasePriceValue',
+    { xpath: '/document/*/originalObjectPurchasePriceCurrency', transform: ->(text) { CSURN.parse(text)[:label] } },
+    '/document/*/originalObjectPurchasePriceValue',
+    '/document/*/transferOfTitleNumber',
     '/document/*/fieldCollectionEventNames/fieldCollectionEventName',
+    #{ xpath: '/document/*/approvalGroupList/approvalGroup/approvalGroup', transform: ->(text) { CSURN.parse(text)[:label] } },
+    #{ xpath: '/document/*/approvalGroupList/approvalGroup/approvalIndividual', transform: ->(text) { CSURN.parse(text)[:label] } },
+    #{ xpath: '/document/*/approvalGroupList/approvalGroup/approvalStatus', transform: ->(text) { CSURN.parse(text)[:label] } },
+    '/document/*/approvalGroupList/approvalGroup/approvalDate',
+    '/document/*/approvalGroupList/approvalGroup/approvalNote',
   ]}
+
+  context 'sample data row 1' do
+  it "Maps attributes correctly" do
+    test_converter(doc, record, xpaths)
+  end
+ end
+  context 'sample data row 32' do
+      let(:attributes) { get_attributes_by_row('core', 'acquisition_core_all.csv', 32) }
+      let(:coreacquisition) { CoreAcquisition.new(attributes) }
+      let(:doc) { get_doc(coreacquisition) }
+      let(:record) { get_fixture('core_acquisition_row31.xml') }
+      let(:xpaths) {[
+        '/document/*/acquisitionReferenceNumber',
+      ]}
 
   it "Maps attributes correctly" do
     test_converter(doc, record, xpaths)
   end
+ end
 end
