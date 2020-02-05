172386ee5b817f438239a33dd03ccf590dc8850b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-02-05 16:53:24 -0500

    no extra test output!

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -59,10 +59,10 @@ module Helpers
 #      puts xpath
       doc_text = get_text(doc, xpath)
       record_text = get_text(record, xpath)
-      unless doc_text == record_text
-        puts "CONVERTER RESULT for #{xpath}: #{doc_text}"
-        puts "EXPECTED RESULT for #{xpath}: #{record_text}"
-      end
+#      unless doc_text == record_text
+#        puts "CONVERTER RESULT for #{xpath}: #{doc_text}"
+#        puts "EXPECTED RESULT for #{xpath}: #{record_text}"
+#      end
       expect(doc_text).not_to be_empty, -> { "Xpath for doc was empty: #{xpath}" }
       expect(record_text).not_to be_empty, -> { "Xpath for record was empty: #{xpath}" }
       expect(doc_text).to eq(record_text), -> { "Xpath match failure: #{xpath}\n#{doc_text}\n#{record_text}" }
