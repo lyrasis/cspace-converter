398166be176d95580f0bfb78c0a1e375a8331668
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-02-10 16:46:39 +0000

    adding fields

--- lib/collectionspace/converter/core/collectionobject.rb
+++ lib/collectionspace/converter/core/collectionobject.rb
@@ -19,20 +19,32 @@ module CollectionSpace
             'editionnumber' => 'editionNumber',
             'phase' => 'phase',
             'sex' => 'sex',
-            'productionnote' => 'objectProductionNote',
+            'objectproductionnote' => 'objectProductionNote',
             'fieldcollectionnote' => 'fieldCollectionNote',
             'fieldcollectionfeature' => 'fieldCollectionFeature',
-            'distinguishingfeatures' => 'distinguishingFeatures'
+            'distinguishingfeatures' => 'distinguishingFeatures',
+            'agequalifier' => 'ageQualifier',
+            'age' => 'age',
+            'ageunit' => 'ageUnit',
+            'physicaldescription' => 'physicalDescription',
+            'objecthistorynote' => 'objectHistoryNote',
+            'ownershipaccess' => 'ownershipAccess',
+            'ownershipcategory' => 'ownershipCategory',
+            'ownershipplace' => 'ownershipPlace',
+            'ownershipexchangemethod' => 'ownershipExchangeMethod',
+            'ownershipexchangenote' => 'ownershipExchangeNote',
+            'ownershipexchangepricecurrency' => 'ownershipExchangePriceCurrency',
+            'ownershipexchangepricevalue' => 'ownershipExchangePriceValue',
+            'contentdescription' => 'contentDescription',
+            'contentdategroup' => 'contentDateGroup'
+          }
+          pairs_transforms = {
+            'agequalifier' => {'vocab' => 'agequalifier'},
+            'ownershipexchangepricecurrency' => {'vocab' => 'currency'},
+            'contentdategroup' => {'special' => 'structured_date'}
           }
           CSXML::Helpers.add_title(xml, attributes)
-          CSXML::Helpers.add_pairs(xml, attributes, pairs)
-          simple_groups = {
-            'material' => 'material',
-            'productionplace' => 'objectProductionPlace',
-            'techattribute' => 'technicalAttribute',
-            'technique' => 'technique'
-          }
-          CSXML::Helpers.add_simple_groups(xml, attributes, simple_groups)
+          CSXML::Helpers.add_pairs(xml, attributes, pairs, pairs_transforms)
           repeats = {
             'briefdescription' => ['briefDescriptions', 'briefDescription'],
             'comments' => ['comments', 'comment'],
@@ -44,21 +56,51 @@ module CollectionSpace
             'objectstatus' => ['objectStatusList', 'objectStatus'],
             'contentperson' => ['contentPersons', 'contentPerson'],
             'inventorystatus' => ['inventoryStatusList', 'inventoryStatus'],
-            'publishto' => ['publishToList', 'publishTo']
-
+            'publishto' => ['publishToList', 'publishTo'],
+            'objectproductionreason' => ['objectProductionReasons', 'objectProductionReason'],
+            'objectproductiondate' => ['objectProductionDateGroupList', 'objectProductionDateGroup'],
+            'ownerperson' => ['owners', 'owner'],
+            'ownerorganization' => ['owners', 'owner'],
+            'ownershipdate' => ['ownershipDateGroupList', 'ownershipDateGroup'],
+            'contentpeople' => ['contentPeoples', 'contentPeople'],
+            'contentplace' => ['contentPlaces', 'contentPlace'],
+            'contentscript' => ['contentScripts', 'contentScript'],
+            'contentorganization' => ['contentOrganizations', 'contentOrganization'],
+            'contentlanguage' => ['contentLanguages', 'contentLanguage'],
+            'contentactivity' => ['contentActivities', 'contentActivity'],
+            'contentposition' => ['contentPositions', 'contentPosition'], 
+            'contentconcept' => ['contentConcepts', 'contentConcept']
           }
           repeatstransforms = {
             'contentperson' => {'authority' => ['personauthorities', 'person']},
             'inventorystatus' => {'vocab' => 'inventorystatus'},
-            'publishto' => {'vocab' => 'publishto'}
+            'publishto' => {'vocab' => 'publishto'},
+            'objectproductiondate' => {'special' => 'structured_date'},
+            'ownerperson' => {'authority' => ['personauthorities', 'person']},
+            'ownerorganization' => {'authority' => ['orgauthorities', 'organization']},
+            'ownershipdate' => {'special' => 'structured_date'},
+            'contentorganization' => {'authority' => ['orgauthorities', 'organization']},
+            'contentlanguage' => {'vocab' => 'languages'}
           }
           CSXML::Helpers.add_repeats(xml, attributes, repeats, repeatstransforms)
           #measuredPartGroupList, measuredPartGroup 
           CSXML::Helpers.add_measured_part_group_list(xml, attributes)
           #objectProductionDateGroupList, objectProductionDateGroup 
-          CSXML::Helpers.add_date_group_list(
-            xml, 'objectProductionDate', [CSDTP.parse(attributes['productiondate'])]
+=begin
+          objectproductiondate_data = {
+            'objectproductiondate' => 'objectProductionDate',
+          }
+          objectproductiondate_transforms = {
+            'objectproductiondate' => {'special' => 'structured_date'}
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'objectProductionDate',
+            objectproductiondate_data,
+            objectproductiondate_transforms
           )
+=end
           #textualInscriptionGroupList,textualInscriptionGroup 
           textualinscriptiondata = {
             'inscriber' => 'inscriptionContentInscriber',
@@ -76,8 +118,8 @@ module CollectionSpace
           )
           #objectProductionOrganizationGroupList, objectProductionOrganizationGroup
           objectprodorgdata = {
-            'productionorg' => 'objectProductionOrganization',
-            'organizationrole' => 'objectProductionOrganizationRole'
+            'objectproductionorganization' => 'objectProductionOrganization',
+            'objectproductionorganizationrole' => 'objectProductionOrganizationRole'
           }
           objectprodorgtransforms = {
             'productionorg' => {'authority' => ['orgauthorities', 'organization']}
@@ -106,8 +148,8 @@ module CollectionSpace
           )
           # not simple because 'objectProductionPeople' singularized as 'objectProductionPerson'
           objectprodpeopledata = {
-            'productionpeople' => 'objectProductionPeople',
-            'productionpeoplerole' => 'objectProductionPeopleRole'
+            'objectproductionpeople' => 'objectProductionPeople',
+            'objectproductionpeoplerole' => 'objectProductionPeopleRole'
           } 
           CSXML.add_single_level_group_list(
             xml,
@@ -116,20 +158,39 @@ module CollectionSpace
             objectprodpeopledata,
           )
           #objectNameList, objectNameGroup
-          object_name = []
-          objectname = CSDR.split_mvf attributes, 'objectname'
-          objectname.each_with_index do |obj, index|
-            object_name << {"objectName" => obj}
-          end
-          CSXML.add_list xml, 'objectName', object_name, 'Group'
+          objectname_data = {
+            'objectname' => 'objectName',
+            'objectnamecurrency' => 'objectNameCurrency',
+            'objectnamelevel' => 'objectNameLevel',
+            'objectnamesystem' => 'objectNameSystem',
+            'objectnametype' => 'objectNameType',
+            'objectnamelanguage' => 'objectNameLanguage',
+            'objectnamenote' => 'objectNameNote',
+          }
+          objectname_transforms = {
+            'objectnamelanguage' => {'vocab' => 'languages'}
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'objectName',
+            objectname_data,
+            objectname_transforms,
+            list_suffix: 'List'
+          )
           #otherNumberList, otherNumber
-          other_number = []
-          numbervalue = CSDR.split_mvf attributes, 'numbervalue'
-          numbertype = CSDR.split_mvf attributes, 'numbertype'
-          numbervalue.each_with_index do |numval, index|
-            other_number << {"numberValue" => numval, "numberType" => numbertype[index]}
-          end
-          CSXML.add_list xml, 'otherNumber', other_number
+          othernumber_data = {
+            'numbervalue' => 'numberValue',
+            'numbertype' => 'numberType'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'otherNumber',
+            othernumber_data,
+            list_suffix: 'List',
+            group_suffix: ''
+          )
           #assocPeopleGroupList, assocPeopleGroup
           assocpeopledata = {
             'assocpeople' => 'assocPeople',
@@ -143,13 +204,106 @@ module CollectionSpace
           )
           #objectComponentGroupList, objectComponentGroup
           objectcompdata = {
-            'objectcomponentname' => 'objectComponentName'
+            'objectcomponentname' => 'objectComponentName',
+            'objectcomponentinformation' => 'objectComponentInformation'
           }
           CSXML.add_single_level_group_list(
             xml,
             attributes,
             'objectComponent',
             objectcompdata,
+          )
+          #materialGroupList, materialGroup
+          materialdata = {
+            'materialname' => 'materialName',
+            'material' => 'material',
+            'materialcomponent' => 'materialComponent',
+            'materialcomponentnote' => 'materialComponentNote',
+            'materialsource' => 'materialSource'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'material',
+            materialdata
+          )
+          #technicalAttributeGroupList, technicalAttributeGroup
+          techattribute_data = {
+            'technicalattribute' => 'technicalAttribute',
+            'technicalattributemeasurementunit' => 'technicalAttributeMeasurementUnit',
+            'technicalattributemeasurement' => 'technicalAttributeMeasurement'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'technicalAttribute',
+            techattribute_data
+          )
+          #techniqueGroupList, techniqueGroup
+          technique_data = {
+            'technique' => 'technique',
+            'techniquetype' => 'techniqueType'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'technique',
+            technique_data
+          )
+          #objectProductionPlaceGroupList, objectProductionPlaceGroup
+          prodplace_data = {
+            'objectproductionplace' => 'objectProductionPlace',
+            'objectproductionplacerole' => 'objectProductionPlaceRole'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'objectProductionPlace',
+            prodplace_data
+          )
+          #usageGroupList, usageGroup
+          usage_data = {
+            'usage' => 'usage',
+            'usagenote' => 'usageNote'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'usage',
+            usage_data
+          )
+          #contentEventNameGroupList, contentEventNameGroup
+          contenteventname_data = {
+            'contenteventname' => 'contentEventName',
+            'contenteventnametype' => 'contentEventNameType'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'contentEventName',
+            contenteventname_data
+          )
+          #contentOtherGroupList, contentOtherGroup
+          contentother_data = {
+            'contentother' => 'contentOther',
+            'contentothertype' => 'contentOtherType'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'contentOther',
+            contentother_data
+          )
+          #contentObjectGroupList, contentObjectGroup
+          contentobject_data = {
+            'contentobject' => 'contentObject',
+            'contentobjecttype' => 'contentObjectType'
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'contentObject',
+            contentobject_data
           )
         end
       end
