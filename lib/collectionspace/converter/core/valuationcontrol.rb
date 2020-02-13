90c7258f465cce71d9f4053c6bbdfc681dde0f1d
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-02-13 16:59:17 +0000

    adding fields

--- lib/collectionspace/converter/core/valuationcontrol.rb
+++ lib/collectionspace/converter/core/valuationcontrol.rb
@@ -10,7 +10,38 @@ module CollectionSpace
         end
 
         def self.map(xml, attributes)
-          # TODO
+          pairs = {
+            'valuationcontrolrefnumber' => 'valuationcontrolRefNumber',
+            'valuedate' => 'valueDate',
+            'valuenote' => 'valueNote',
+            'valuerenewaldate' => 'valueRenewalDate',
+            'valuetype' => 'valueType',
+            'valuesourceorganization' => 'valueSource',
+            'valuesourceperson' => 'valueSource',
+          }
+          pairstransforms = {
+            'valuedate' => {'special' => 'unstructured_date_stamp'},
+            'valuerenewaldate' => {'special' => 'unstructured_date_stamp'},
+            'valuesourceorganization' => {'authority' => ['orgauthorities', 'organization']},
+            'valuesourceperson' => {'authority' => ['personauthorities', 'person']}
+          }
+          CSXML::Helpers.add_pairs(xml, attributes, pairs, pairstransforms)
+          #valueAmountsList, valueAmounts
+          valueamounts_data = {
+            'valuecurrency' => 'valueCurrency',
+            'valueamount' => 'valueAmount'
+          }
+          valueamounts_transforms = {
+            'valuecurrency' => {'vocab' => 'currency'}
+          }
+          CSXML.add_single_level_group_list(
+            xml, attributes,
+            'valueAmounts',
+            valueamounts_data,
+            valueamounts_transforms,
+            list_suffix: 'List',
+            group_suffix: ''
+          )
         end
       end
     end
