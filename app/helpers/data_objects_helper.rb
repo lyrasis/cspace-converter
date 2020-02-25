32a64d47832f749902dc3d868b224f9419bed094
Mark Cooper <markchristophercooper@gmail.com>
2020-02-25 08:27:42 -0800

    Retain batch params and highlight status

--- app/helpers/data_objects_helper.rb
+++ app/helpers/data_objects_helper.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module DataObjectsHelper
-  def current_page_params
-    request.params.slice('batch', 'errors', 'filters', 'page')
-  end
-
   def identifier_for(object)
     if object.read_attribute(:identify_by_column)
       object.csv_data.fetch(object.identify_by_column, object.id)
@@ -17,7 +13,11 @@ module DataObjectsHelper
     "#{object.id} #{object.import_batch} #{object.converter_module} #{object.converter_profile}"
   end
 
-  def status_for(object)
+  def status_class(object)
+    object.import_status.zero? ? 'table-danger' : 'table-default'
+  end
+
+  def status(object)
     object.import_status.zero? ? 'fail' : 'ok'
   end
 end
