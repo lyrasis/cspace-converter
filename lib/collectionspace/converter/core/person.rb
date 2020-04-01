6638a4da777138f2362bd733b4ddc6e52a68bfc2
Mark Cooper <markchristophercooper@gmail.com>
2020-04-01 15:32:47 -0700

    WS cleanup

--- lib/collectionspace/converter/core/person.rb
+++ lib/collectionspace/converter/core/person.rb
@@ -34,7 +34,7 @@ module CollectionSpace
             'namenote' => 'nameNote'
           }
           CSXML::Helpers.add_pairs(xml, attributes, pairs)
-    
+
           repeats = { 
             'group' => ['groups', 'group'],
             'nationality' => ['nationalities', 'nationality'],
@@ -43,31 +43,30 @@ module CollectionSpace
           }
           CSXML::Helpers.add_repeats(xml, attributes, repeats)
 
-
           CSXML.add xml, 'shortIdentifier', config[:identifier]
           #personTermGroupList, personTermGroup
           personterm_data = {
             "title"  => "title", 
-	    "initials" => "initials",
-	    "forename" => "foreName",
-	    "surname" => "surName",
-	    "nameadditions" => "nameAdditions",
-	    "middlename" => "middleName",
-	    "salutation" => "salutation",
-	    "termdisplayname" => "termDisplayName",
-	    "termlanguage" => "termLanguage",
-	    "termname" => "termName",
-	    "termprefforlang" => "termPrefForLang",
-	    "termqualifier" => "termQualifier",
-	    "termsource" => "termSource",
-	    "termsourceid" => "termSourceID",
-	    "termsourcedetail" => "termSourceDetail",
-	    "termsourcenote" => "termSourceNote",
- 	    "termstatus" => "termStatus",
-	    "termtype" => "termType",
+            "initials" => "initials",
+            "forename" => "foreName",
+            "surname" => "surName",
+            "nameadditions" => "nameAdditions",
+            "middlename" => "middleName",
+            "salutation" => "salutation",
+            "termdisplayname" => "termDisplayName",
+            "termlanguage" => "termLanguage",
+            "termname" => "termName",
+            "termprefforlang" => "termPrefForLang",
+            "termqualifier" => "termQualifier",
+            "termsource" => "termSource",
+            "termsourceid" => "termSourceID",
+            "termsourcedetail" => "termSourceDetail",
+            "termsourcenote" => "termSourceNote",
+            "termstatus" => "termStatus",
+            "termtype" => "termType",
             "termflag" => "termFlag",
             "termdisplaynamenonpreferred" => "termFormattedDisplayName"
-	  }
+          }
           personterm_transforms = {
             'termlanguage' => {'vocab' => 'languages'},
             'termsource' => {'vocab' => 'citation'},
