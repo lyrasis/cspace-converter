1eeea6d6806b6bcfe1f3c28f82b9b9a129ccc65c
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-05-07 08:59:42 +0000

    adding new fields

--- lib/collectionspace/converter/publicart/movement.rb
+++ lib/collectionspace/converter/publicart/movement.rb
@@ -5,12 +5,39 @@ module CollectionSpace
         ::PublicArtMovement = CollectionSpace::Converter::PublicArt::PublicArtMovement
         def convert
           run do |xml|
-            CoreMovement.map(xml, attributes)
+            PublicArtMovement.map(xml, attributes)
           end
         end
 
         def self.map(xml, attributes)
-          # n/a
+          pairs = {
+            'movementreferencenumber' => 'movementReferenceNumber',
+            'currentlocationplace' => 'currentLocation',
+            'currentlocationorganization' => 'currentLocation',
+            'currentlocationstorage' => 'currentLocation',
+            'currentlocationfitness' => 'currentLocationFitness',
+            'currentlocationnote' => 'currentLocationNote',
+            'locationdate' => 'locationDate',
+            'reasonformove' => 'reasonForMove',
+            'movementcontact' => 'movementContact',
+            'movementnote' => 'movementNote',
+            'plannedremovaldate' => 'plannedRemovalDate',
+            'removaldate' => 'removalDate'
+          }
+          pairstransforms = {
+            'currentlocationplace' => {'authority' => ['placeauthorities', 'place']},
+            'currentlocationstorage' => {'authority' => ['locationauthorities', 'location']},
+            'currentlocationorganization' => {'authority' => ['orgauthorities', 'organization']},
+            'locationdate' => {'special' => 'unstructured_date_stamp'},
+            'movementcontact' => {'authority' => ['personauthorities', 'person']},
+            'removaldate' => {'special' => 'unstructured_date_stamp'}
+          }
+          CSXML::Helpers.add_pairs(xml, attributes, pairs, pairstransforms)
+          repeats = {
+            'movementmethod' => ['movementMethods', 'movementMethod'],
+
+          }
+          CSXML::Helpers.add_repeats(xml, attributes, repeats)
         end
       end
     end
