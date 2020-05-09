b376d0d24f899edcd1cf0ee0cb80978d1e600444
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-05-09 19:13:39 +0000

    adding new fields

--- lib/collectionspace/converter/publicart/person.rb
+++ lib/collectionspace/converter/publicart/person.rb
@@ -10,17 +10,15 @@ module CollectionSpace
                 "xmlns:ns2" => "http://collectionspace.org/services/person",
                 "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
             ) do
-              # applying namespace breaks import
               xml.parent.namespace = nil
-              CorePerson.map(xml, attributes)
+              PublicArtPerson.map(xml, attributes, config)
             end
 
             xml.send(
-                "ns2:persons_publicart",
-                "xmlns:ns2" => "http://collectionspace.org/services/person/local/publicart",
-                "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
+              'ns2:persons_publicart',
+              'xmlns:ns2' => 'http://collectionspace.org/services/person/local/publicart',
+              'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance'
             ) do
-              # applying namespace breaks import
               xml.parent.namespace = nil
               PublicArtPerson.extension(xml, attributes)
             end
@@ -30,25 +28,92 @@ module CollectionSpace
                 "xmlns:ns2" => "http://collectionspace.org/services/contact",
                 "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
             ) do
-              # applying namespace breaks import
               xml.parent.namespace = nil
               Contact.map(xml, attributes)
             end
           end
         end
 
+        def self.map(xml, attributes, config)
+          pairs = {
+            'birthplace' => 'birthPlace',
+            'deathplace' => 'deathPlace',
+            'bionote' => 'bioNote'
+          }
+          CSXML::Helpers.add_pairs(xml, attributes, pairs)
+
+          CSXML.add xml, 'shortIdentifier', config[:identifier]
+          #personTermGroupList, personTermGroup
+          personterm_data = {
+            "title"  => "title", 
+            "initials" => "initials",
+            "forename" => "foreName",
+            "surname" => "surName",
+            "nameadditions" => "nameAdditions",
+            "middlename" => "middleName",
+            "salutation" => "salutation",
+            "termdisplayname" => "termDisplayName",
+            "termlanguage" => "termLanguage",
+            "termname" => "termName",
+            "termprefforlang" => "termPrefForLang",
+            "termqualifier" => "termQualifier",
+            "termsource" => "termSource",
+            "termsourceid" => "termSourceID",
+            "termsourcedetail" => "termSourceDetail",
+            "termsourcenote" => "termSourceNote",
+            "termstatus" => "termStatus",
+            "termtype" => "termType",
+            "termflag" => "termFlag",
+            "termdisplaynamenonpreferred" => "termFormattedDisplayName"
+          }
+          personterm_transforms = {
+            'termlanguage' => {'vocab' => 'languages'},
+            'termsource' => {'vocab' => 'citation'},
+            'termtype' => {'vocab' => 'persontermtype'},
+            'termflag' => {'vocab' => 'persontermflag'}
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'personTerm',
+            personterm_data,
+            personterm_transforms
+          )
+          #birthDateGroup
+          CSXML::Helpers.add_date_group(
+            xml, 'birthDate', CSDTP.parse(attributes['birthdategroup'])
+          )
+          #deathDateGroup
+          CSXML::Helpers.add_date_group(
+            xml, 'deathDate', CSDTP.parse(attributes['deathdategroup'])
+          )
+        end
+  
         def self.extension(xml, attributes)
-          organization_urns = []
-          organizations = split_mvf attributes, 'organization'
-          organizations.each do |organization|
-            organization_urns << { "organization" => CSURN.get_authority_urn('orgauthorities', 'organization', organization, true) }
-          end
-          CSXML.add_repeat(xml, 'organizations', organization_urns)
+          repeats = { 
+            'organization' => ['organizations', 'organization']
+          }
+          repeats_transforms = {
+            'organization' => {'authority' => ['orgauthorities', 'organization']}
+          }
+          CSXML::Helpers.add_repeats(xml, attributes, repeats, repeats_transforms)
+          #socialMediaGroupList, socialMediaGroup
+          socialmedia_data = {
+            "socialmediahandle" => "socialMediaHandle",
+            "socialmediahandletype" => "socialMediaHandleType",
+          }
+          socialmedia_transforms = {
+            'socialmediahandletype' => {'vocab' => 'socialmediatype'}
+          }
+          CSXML.add_single_level_group_list(
+            xml,
+            attributes,
+            'socialMedia',
+            socialmedia_data,
+            socialmedia_transforms
+          )
         end
 
-        def self.map(xml, attributes)
-          # n/a
-        end
       end
     end
   end
