b276bb8e268d54becbc86f9e3c8a6d935ef46b9d
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-05-14 08:20:10 +0000

    updating redefined fields

--- lib/collectionspace/converter/publicart/movement.rb
+++ lib/collectionspace/converter/publicart/movement.rb
@@ -3,42 +3,61 @@ module CollectionSpace
     module PublicArt
       class PublicArtMovement < Movement
         ::PublicArtMovement = CollectionSpace::Converter::PublicArt::PublicArtMovement
+         def redefined_fields
+          @redefined.concat([
+            # not in publicart
+            'normalLocation',
+            'inventoryActionRequired',
+            'frequencyForInventory',
+            'inventoryDate',
+            'nextInventoryDate',
+            'inventoryContact',
+            'inventoryNote',
+            # overridden by publicart
+            'currentLocation',
+            'movementContact'
+          ])
+          super
+        end
+
         def convert
-          run do |xml|
-            PublicArtMovement.map(xml, attributes)
+          run(wrapper: 'document') do |xml|
+            xml.send(
+              'ns2:movements_common',
+              'xmlns:ns2' => 'http://collectionspace.org/services/movement',
+              'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance'
+            ) do
+              xml.parent.namespace = nil
+              PublicArtMovement.map_common(xml, attributes, redefined_fields)
+            end
+
           end
         end
 
-        def self.map(xml, attributes)
-          pairs = {
-            'movementreferencenumber' => 'movementReferenceNumber',
-            'currentlocationplace' => 'currentLocation',
-            'currentlocationorganization' => 'currentLocation',
-            'currentlocationstorage' => 'currentLocation',
-            'currentlocationfitness' => 'currentLocationFitness',
-            'currentlocationnote' => 'currentLocationNote',
-            'locationdate' => 'locationDate',
-            'reasonformove' => 'reasonForMove',
-            'movementcontact' => 'movementContact',
-            'movementnote' => 'movementNote',
-            'plannedremovaldate' => 'plannedRemovalDate',
-            'removaldate' => 'removalDate'
-          }
-          pairstransforms = {
-            'currentlocationplace' => {'authority' => ['placeauthorities', 'place']},
-            'currentlocationstorage' => {'authority' => ['locationauthorities', 'location']},
-            'currentlocationorganization' => {'authority' => ['orgauthorities', 'organization']},
-            'locationdate' => {'special' => 'unstructured_date_stamp'},
-            'movementcontact' => {'authority' => ['personauthorities', 'person']},
-            'removaldate' => {'special' => 'unstructured_date_stamp'},
-            'plannedremovaldate' => {'special' => 'unstructured_date_stamp'}
-          }
-          CSXML::Helpers.add_pairs(xml, attributes, pairs, pairstransforms)
-          repeats = {
-            'movementmethod' => ['movementMethods', 'movementMethod'],
 
+        def self.map_common(xml, attributes, redefined)
+          CoreMovement.map_common(xml, attributes.merge(redefined))
+          pairs = {
+            'currentlocationstorage' => 'currentLocation',
+            'currentlocationorganization' => 'currentLocation',
+            'currentlocationplace' => 'currentLocation',
+            'currentlocationstorageoffsite' => 'currentLocation',
+            'currentlocationorganizationshared' => 'currentLocation',
+            'currentlocationplaceshared' => 'currentLocation',
+            'movementcontact' => 'movementContact',
+            'movementcontactshared' => 'movementContact'
           }
-          CSXML::Helpers.add_repeats(xml, attributes, repeats)
+          pairs_transforms = {
+            'currentlocationstoragelocal' => {'authority' => ['locationauthorities', 'location']},
+            'currentlocationorganizationlocal' => {'authority' => ['orgauthorities', 'organization']},
+            'currentlocationplacelocal' => {'authority' => ['placeauthorities', 'place']},
+            'currentlocationstorageoffsite' => {'authority' => ['locationauthorities', 'offsite_sla']},
+            'currentlocationorganizationshared' => {'authority' => ['orgauthorities', 'organization_shared']},
+            'currentlocationplaceshared' => {'authority' => ['placeauthorities', 'place_shared']},
+            'movementcontact' => {'authority' => ['personauthorities', 'person']},
+            'movementcontactshared' => {'authority' => ['personauthorities', 'person_shared']}
+          }
+          CSXML::Helpers.add_pairs(xml, attributes, pairs, pairs_transforms)
         end
       end
     end
