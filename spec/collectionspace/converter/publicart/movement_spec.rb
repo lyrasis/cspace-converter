2e1a2e3db7f80c117dd83ce8fd6315645b01b417
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-05-18 10:23:47 +0000

    updating redefined fields

--- spec/collectionspace/converter/publicart/movement_spec.rb
+++ spec/collectionspace/converter/publicart/movement_spec.rb
@@ -2,39 +2,66 @@ require 'rails_helper'
 
 RSpec.describe CollectionSpace::Converter::PublicArt::PublicArtMovement do
   let(:attributes) { get_attributes('publicart', 'lmi_publicart_all.csv') }
-  let(:publicartmovement) { PublicArtMovement.new(attributes) }
-  let(:doc) { Nokogiri::XML(publicartmovement.convert, nil, 'UTF-8') }
+  let(:publicartmovement) { PublicArtMovement.new(Lookup.profile_defaults('movement').merge(attributes)) }
+  let(:doc) { get_doc(publicartmovement) }
   let(:record) { get_fixture('publicart_movement.xml') }
-  let(:xpaths) {[
-    '/document/*/movementReferenceNumber',
-    { xpath: '/document/*/currentLocation', transform: ->(text) { CSURN.parse(text)[:label] } },
-    '/document/*/currentLocationFitness',
-    '/document/*/currentLocationNote',
-    '/document/*/locationDate',
-    '/document/*/reasonForMove',
-    { xpath: '/document/*/movementContact', transform: ->(text) { CSURN.parse(text)[:label] } },
-    '/document/*/movementMethods/movementMethod',
-    '/document/*/plannedRemovalDate',
-    '/document/*/removalDate',
-    '/document/*/movementNote',
-  ]}
- 
-  context 'For maximally populuated record' do
-    it "Maps attributes correctly" do
-      test_converter(doc, record, xpaths)
+
+  describe 'map_common' do
+    p = 'movements_common'
+    context 'fields not included in publicart' do
+      [
+        "/document/#{p}/normalLocation",
+        "/document/#{p}/inventoryActionRequired",
+        "/document/#{p}/frequencyForInventory",
+        "/document/#{p}/inventoryDate",
+        "/document/#{p}/nextInventoryDate",
+        "/document/#{p}/inventoryContactList/inventoryContact",
+        "/document/#{p}/inventoryNote",
+      ].each do |xpath|
+        context "for xpath: #{xpath}" do
+          it 'is empty' do
+            expect(get_text(doc, xpath)).to be_empty
+          end
+        end
+      end
     end
-  end
 
-  context 'For minimally populated record' do
-    let(:attributes) { get_attributes_by_row('publicart', 'lmi_publicart_all.csv', 11) }
-    let(:doc) { Nokogiri::XML(publicartmovement.convert, nil, 'UTF-8') }
-    let(:record) { get_fixture('publicart_movement_row11.xml') }
-    let(:xpath_required) {[
-      { xpath: '/document/*/currentLocation', transform: ->(text) { CSURN.parse(text)[:label] } },
-    ]}
-
-    it 'Maps required field(s) correctly without falling over' do
-      test_converter(doc, record, xpath_required)
+    context 'fields overridden by publicart' do
+      context 'when local auth/vocab' do
+        [
+          "/document/#{p}/currentLocation",
+          "/document/#{p}/movementContact"
+        ].each do |xpath|
+          context "#{xpath}" do
+            it 'all values will be URNs' do
+              expect(urn_values(doc, xpath)).not_to include('Not a URN')
+            end
+            
+            it 'URNs match sample payload' do
+              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+            end
+          end
+        end
+      end
+      context 'when shared auth/vocab' do
+        let(:attributes) { get_attributes_by_row('publicart', 'lmi_publicart_all.csv', 3) }
+        let(:doc) { get_doc(publicartmovement) }
+        let(:record) { get_fixture('publicart_movement_row3.xml') }
+        [
+          "/document/#{p}/currentLocation",
+          "/document/#{p}/movementContact"
+        ].each do |xpath|
+          context "#{xpath}" do
+            it 'all values will be URNs' do
+              expect(urn_values(doc, xpath)).not_to include('Not a URN')
+            end
+            
+            it 'URNs match sample payload' do
+              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+            end
+          end
+        end
+      end
     end
   end
 end
