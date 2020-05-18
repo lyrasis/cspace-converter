9235fffadc4d3be1333a4ae023b2b1e754ea3fff
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-05-18 18:03:33 +0000

    updating overriden field

--- spec/collectionspace/converter/publicart/valuation_spec.rb
+++ spec/collectionspace/converter/publicart/valuation_spec.rb
@@ -2,49 +2,70 @@ require 'rails_helper'
 
 RSpec.describe CollectionSpace::Converter::PublicArt::PublicArtValuationControl do
   let(:attributes) { get_attributes('publicart', 'valuationcontrol_publicart_all.csv') }
-  let(:publicartvaluationcontrol) { PublicArtValuationControl.new(attributes) }
-  let(:doc) { Nokogiri::XML(publicartvaluationcontrol.convert, nil, 'UTF-8') }
+  let(:publicartvaluationcontrol) { PublicArtValuationControl.new(Lookup.profile_defaults('valuationcontrol').merge(attributes)) }
+  let(:doc) { get_doc(publicartvaluationcontrol) }
   let(:record) { get_fixture('publicart_valuation.xml') }
-  let(:p) { 'valuationcontrols_common' }
-  let(:ext) { 'valuationcontrols_publicart' }
-  let(:xpaths) {[
-    "/document/#{p}/valuationcontrolRefNumber",
-    { xpath: "/document/#{p}/valueAmountsList/valueAmounts[1]/valueCurrency", transform: ->(text) { CSURN.parse(text)[:label].downcase } },
-    { xpath: "/document/#{p}/valueAmountsList/valueAmounts[1]/valueCurrency", transform: ->(text) { CSURN.parse(text)[:subtype].downcase } },
-    { xpath: "/document/#{p}/valueAmountsList/valueAmounts[2]/valueCurrency", transform: ->(text) { CSURN.parse(text)[:label].downcase } },
-    { xpath: "/document/#{p}/valueAmountsList/valueAmounts[2]/valueCurrency", transform: ->(text) { CSURN.parse(text)[:subtype].downcase } },
-    "/document/#{p}/valueAmountsList/valueAmounts/valueAmount",
-    "/document/#{p}/valueDate",
-    "/document/#{p}/valueNote",
-    "/document/#{p}/valueRenewalDate",
-    "/document/#{p}/valueType",
-    { xpath: "/document/#{p}/valueSource", transform: ->(text) { CSURN.parse(text)[:label] } },
-    { xpath: "/document/#{p}/valueSource", transform: ->(text) { CSURN.parse(text)[:subtype] } },
-    { xpath: "/document/#{ext}/valueSourceRole", transform: ->(text) { CSURN.parse(text)[:label].downcase } },
-    { xpath: "/document/#{ext}/valueSourceRole", transform: ->(text) { CSURN.parse(text)[:subtype].downcase } },
-    "/document/#{ext}/insuranceGroupList/insuranceGroup/insuranceNote",
-    "/document/#{ext}/insuranceGroupList/insuranceGroup/insuranceRenewalDate",
-    { xpath: "/document/#{ext}/insuranceGroupList/insuranceGroup[1]/insurer", transform: ->(text) { CSURN.parse(text)[:label] } },
-    { xpath: "/document/#{ext}/insuranceGroupList/insuranceGroup[1]/insurer", transform: ->(text) { CSURN.parse(text)[:subtype] } },
-    { xpath: "/document/#{ext}/insuranceGroupList/insuranceGroup[2]/insurer", transform: ->(text) { CSURN.parse(text)[:label] } },
-    { xpath: "/document/#{ext}/insuranceGroupList/insuranceGroup[2]/insurer", transform: ->(text) { CSURN.parse(text)[:subtype] } },
-    "/document/#{ext}/insuranceGroupList/insuranceGroup/insurancePolicyNumber",
-  ]}
-  context 'For maximally populuated record' do
-    it "Maps attributes correctly" do
-      test_converter(doc, record, xpaths)
+
+  describe 'map_common' do
+    p = 'valuationcontrols_common'
+    context 'fields overridden by publicart' do
+      context 'when local value source' do
+        [
+          "/document/#{p}/valueSource"
+        ].each do |xpath|
+          context "#{xpath}" do
+            it 'all values will be URNs' do
+              expect(urn_values(doc, xpath)).not_to include('Not a URN')
+            end
+            
+            it 'URNs match sample payload' do
+              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+            end
+          end
+        end
+      end
+      context 'when shared value source' do
+        let(:attributes) { get_attributes_by_row('publicart', 'valuationcontrol_publicart_all.csv', 3) }
+        let(:doc) { get_doc(publicartvaluationcontrol) }
+        let(:record) { get_fixture('publicart_valuation_row3.xml') }
+        [
+          "/document/#{p}/valueSource"
+        ].each do |xpath|
+          context "#{xpath}" do
+            it 'all values will be URNs' do
+              expect(urn_values(doc, xpath)).not_to include('Not a URN')
+            end
+            
+            it 'URNs match sample payload' do
+              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+            end
+          end
+        end
+      end
     end
   end
-  context 'For minimally populated record' do
-    let(:attributes) { get_attributes_by_row('publicart', 'valuationcontrol_publicart_all.csv', 22) }
-    let(:doc) { Nokogiri::XML(publicartvaluationcontrol.convert, nil, 'UTF-8') }
-    let(:record) { get_fixture('publicart_valuation_row22.xml') }
-    let(:xpath_required) {[
-      "/document/*/valuationcontrolRefNumber"
-    ]}
 
-    it 'Maps required field(s) correctly without falling over' do
-      test_converter(doc, record, xpath_required)
+  describe 'map_publicart' do
+    pa = 'valuationcontrols_publicart'
+    context 'authority/vocabulary fields' do
+      [
+        "/document/#{pa}/valueSourceRole",
+        "/document/#{pa}/insuranceGroupList/insuranceGroup/insurer",
+        "/document/#{pa}/insuranceGroupList/insuranceGroup/insuranceNote",
+        "/document/#{pa}/insuranceGroupList/insuranceGroup/insurancePolicyNumber",
+        "/document/#{pa}/insuranceGroupList/insuranceGroup/insuranceRenewalDate",
+
+      ].each do |xpath|
+        context "#{xpath}" do
+          it 'all values will be URNs' do
+            expect(urn_values(doc, xpath)).not_to include('Not a URN')
+          end
+          
+          it 'URNs match sample payload' do
+            expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+          end
+        end
+      end      
     end
   end
 end
