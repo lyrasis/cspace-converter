6883a01351f56dfbb3b92d9cd67de66b6e9754d9
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-05-18 18:16:08 -0400

    update publicart org spec

--- spec/collectionspace/converter/publicart/organization_spec.rb
+++ spec/collectionspace/converter/publicart/organization_spec.rb
@@ -14,26 +14,31 @@ RSpec.describe CollectionSpace::Converter::PublicArt::PublicArtOrganization do
         "/document/#{p}/groups/group",
         "/document/#{p}/functions/function"
       ].each do |xpath|
-        context "for xpath: #{xpath}" do
+        context "#{xpath}" do
           it 'is empty' do
-            expect(get_text(doc, xpath)).to be_empty
+            verify_empty_field(doc, xpath)
           end
         end
       end
     end
 
     context 'fields overridden by publicart' do
+      # foundingPlace is not a repeatable field
+      # it does not appear in a repeatablew group
+      # therefore, we need 2 rows in the spreadsheet and 2 separate tests
+      #  to verify the two different term sources get converted properly
       context 'when local place' do
         [
           "/document/#{p}/foundingPlace"
         ].each do |xpath|
           context "#{xpath}" do
-            it 'all values will be URNs' do
-              expect(urn_values(doc, xpath)).not_to include('Not a URN')
+            let(:urn_vals) { urn_values(doc, xpath) }
+            it 'values are URNs' do
+              verify_values_are_urns(urn_vals)
             end
             
             it 'URNs match sample payload' do
-              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+              verify_urn_match(urn_vals, record, xpath)
             end
           end
         end
@@ -46,12 +51,13 @@ RSpec.describe CollectionSpace::Converter::PublicArt::PublicArtOrganization do
           "/document/#{p}/foundingPlace"
         ].each do |xpath|
           context "#{xpath}" do
-            it 'all values will be URNs' do
-              expect(urn_values(doc, xpath)).not_to include('Not a URN')
+            let(:urn_vals) { urn_values(doc, xpath) }
+            it 'values are URNs' do
+              verify_values_are_urns(urn_vals)
             end
             
             it 'URNs match sample payload' do
-              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+              verify_urn_match(urn_vals, record, xpath)
             end
           end
         end
@@ -66,15 +72,16 @@ RSpec.describe CollectionSpace::Converter::PublicArt::PublicArtOrganization do
         "/document/#{pa}/placementType",
         "/document/#{pa}/currentPlace"
       ].each do |xpath|
-        context "#{xpath}" do
-          it 'all values will be URNs' do
-            expect(urn_values(doc, xpath)).not_to include('Not a URN')
+          context "#{xpath}" do
+            let(:urn_vals) { urn_values(doc, xpath) }
+            it 'values are URNs' do
+              verify_values_are_urns(urn_vals)
+            end
+            
+            it 'URNs match sample payload' do
+              verify_urn_match(urn_vals, record, xpath)
+            end
           end
-          
-          it 'URNs match sample payload' do
-            expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
-          end
-        end
       end      
     end
   end
