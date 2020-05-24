dbc0d10d474a836c7664231492765c68a22a8a98
Paul Mulonzia <paulmwendwa43@gmail.com>
2020-05-24 20:50:09 +0000

    spec updates

--- spec/collectionspace/converter/publicart/movement_spec.rb
+++ spec/collectionspace/converter/publicart/movement_spec.rb
@@ -18,50 +18,36 @@ RSpec.describe CollectionSpace::Converter::PublicArt::PublicArtMovement do
         "/document/#{p}/inventoryContactList/inventoryContact",
         "/document/#{p}/inventoryNote",
       ].each do |xpath|
-        context "for xpath: #{xpath}" do
+        context "#{xpath}" do
           it 'is empty' do
-            expect(get_text(doc, xpath)).to be_empty
+            verify_field_is_empty(doc, xpath)
           end
         end
       end
     end
 
     context 'fields overridden by publicart' do
-      context 'when local auth/vocab' do
+      context 'authority/vocab fields' do
         [
           "/document/#{p}/currentLocation",
           "/document/#{p}/movementContact"
         ].each do |xpath|
           context "#{xpath}" do
-            it 'all values will be URNs' do
-              expect(urn_values(doc, xpath)).not_to include('Not a URN')
+            let(:urn_vals) { urn_values(doc, xpath) }
+            it 'is not empty' do
+              verify_field_is_populated(doc, xpath)
+            end
+
+            it 'values are URNs' do
+              verify_values_are_urns(urn_vals)
             end
             
             it 'URNs match sample payload' do
-              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
+              verify_urn_match(urn_vals, record, xpath)
             end
           end
         end
-      end
-      context 'when shared auth/vocab' do
-        let(:attributes) { get_attributes_by_row('publicart', 'lmi_publicart_all.csv', 3) }
-        let(:doc) { get_doc(publicartmovement) }
-        let(:record) { get_fixture('publicart_movement_row3.xml') }
-        [
-          "/document/#{p}/currentLocation",
-          "/document/#{p}/movementContact"
-        ].each do |xpath|
-          context "#{xpath}" do
-            it 'all values will be URNs' do
-              expect(urn_values(doc, xpath)).not_to include('Not a URN')
-            end
-            
-            it 'URNs match sample payload' do
-              expect(urn_values(doc, xpath)).to eq(urn_values(record, xpath))
-            end
-          end
-        end
-      end
+      end      
     end
   end
 end
