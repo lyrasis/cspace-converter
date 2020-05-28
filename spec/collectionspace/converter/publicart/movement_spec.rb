b98ec6f33e599439ec141bc5f2ed6c8acfb5984e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-05-28 11:15:14 -0400

    updates to spec

--- spec/collectionspace/converter/publicart/movement_spec.rb
+++ spec/collectionspace/converter/publicart/movement_spec.rb
@@ -28,26 +28,38 @@ RSpec.describe CollectionSpace::Converter::PublicArt::PublicArtMovement do
 
     context 'fields overridden by publicart' do
       context 'authority/vocab fields' do
+        rows = [2, 3, 11]
+        let(:fixtures) { ['publicart_movement.xml', 'publicart_movement_row3.xml', 'publicart_movement_row11.xml'] }
+        let(:attributes) {
+          rows.map { |r| get_attributes_by_row('publicart', 'lmi_publicart_all.csv', r) }
+            .map{ |r| Lookup.profile_defaults('movement').merge(r) }
+        }
+        let(:objs) { attributes.map{ |attr| PublicArtMovement.new(attr) } }
+        let(:docs) { objs.map{ |obj| get_doc(obj) } }
+        let(:records) { fixtures.map{ |f| get_fixture(f) } }
+
         [
           "/document/#{p}/currentLocation",
           "/document/#{p}/movementContact"
         ].each do |xpath|
-          context "#{xpath}" do
-            let(:urn_vals) { urn_values(doc, xpath) }
-            it 'is not empty' do
-              verify_field_is_populated(doc, xpath)
-            end
+          rows.each_with_index do |row, i|
+            context "row #{row}: #{xpath}" do
+              let(:urn_vals) { urn_values(docs[i], xpath) }
+              it 'is not empty' do
+                verify_field_is_populated(docs[i], xpath)
+              end
 
-            it 'values are URNs' do
-              verify_values_are_urns(urn_vals)
-            end
-            
-            it 'URNs match sample payload' do
-              verify_urn_match(urn_vals, record, xpath)
+              it 'values are URNs' do
+                verify_values_are_urns(urn_vals)
+              end
+              
+              it 'URNs match sample payload' do
+                verify_urn_match(urn_vals, records[i], xpath)
+              end
             end
           end
         end
-      end      
+      end
     end
   end
 end
